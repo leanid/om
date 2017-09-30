5a3f4f82d4eeae915dc038698ddb8bafc6c20d60
leo <leanid.chaika@gmail.com>
2017-09-30 21:16:05 +0300

    добавил начальный пример как делать самый простой тест, собрал под федоркой

---
+++ tests/01-hello-world/main.cxx
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include <iostream>
+
+int main(int /*argc*/, char* /*argv*/ [])
+{
+    using namespace std;
+    using namespace std::literals;
+
+    std::cout << "hello world"s << endl;
+
+    return EXIT_SUCCESS;
+}
