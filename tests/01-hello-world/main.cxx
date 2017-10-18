127e1809146ddf34fae910ec341233c364170750
leo <leanid.chaika@gmail.com>
2017-10-18 19:08:29 +0300

    более сложный пример реальной жизни с проверкой результата

--- tests/01-hello-world/main.cxx
+++ tests/01-hello-world/main.cxx
@@ -4,9 +4,13 @@
 int main(int /*argc*/, char* /*argv*/ [])
 {
     using namespace std;
-    using namespace std::literals;
 
-    std::cout << "hello world"s << endl;
+    string_view output_phrase("hello world");
 
-    return EXIT_SUCCESS;
+    cout << output_phrase << endl;
+
+    bool is_good = cout.good();
+
+    int result = is_good ? EXIT_SUCCESS : EXIT_FAILURE;
+    return result;
 }
