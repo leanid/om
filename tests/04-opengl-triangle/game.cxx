79b798529130da36cf4f06969ddd5004aac20d38
leo <leanid.chaika@gmail.com>
2017-11-25 21:40:46 +0300

    on linux machine mingw and native build works! need test it on windows

---
+++ tests/04-opengl-triangle/game.cxx
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <string_view>
+
+#include "engine.hxx"
+
+int main(int /*argc*/, char* /*argv*/ [])
+{
+    std::unique_ptr<om::engine, void (*)(om::engine*)> engine(
+        om::create_engine(), om::destroy_engine);
+
+    const std::string error = engine->initialize("");
+    if (!error.empty())
+    {
+        std::cerr << error << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    bool continue_loop = true;
+    while (continue_loop)
+    {
+        om::event event;
+
+        while (engine->read_input(event))
+        {
+            std::cout << event << std::endl;
+            switch (event)
+            {
+                case om::event::turn_off:
+                    continue_loop = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        std::ifstream file("vertexes.txt");
+        assert(!!file);
+
+        om::triangle tr;
+        file >> tr;
+
+        engine->render_triangle(tr);
+
+        engine->swap_buffers();
+    }
+
+    engine->uninitialize();
+
+    return EXIT_SUCCESS;
+}
