cca011f9282e7434828abd69b4283444f3c5f205
leanid.chaika <leanid.chaika@gmail.com>
2017-12-08 18:42:05 +0300

    add next tuturial

---
+++ tests/06-1-matrix-and-vector/engine.hxx
@@ -0,0 +1,169 @@
+#include <iosfwd>
+#include <string>
+#include <string_view>
+
+#ifndef OM_DECLSPEC
+#define OM_DECLSPEC
+#endif
+
+namespace om
+{
+
+struct OM_DECLSPEC vec2
+{
+    vec2();
+    vec2(float x, float y);
+    float x = 0;
+    float y = 0;
+};
+
+vec2 OM_DECLSPEC operator+(const vec2& l, const vec2& r);
+
+struct OM_DECLSPEC mat2
+{
+    mat2();
+    static mat2 identiry();
+    static mat2 scale(float scale);
+    static mat2 rotation(float thetha);
+    vec2 col0;
+    vec2 col1;
+};
+
+vec2 OM_DECLSPEC operator*(const vec2& v, const mat2& m);
+mat2 OM_DECLSPEC operator*(const mat2& m1, const mat2& m2);
+
+/// dendy gamepad emulation events
+enum class event
+{
+    /// input events
+    left_pressed,
+    left_released,
+    right_pressed,
+    right_released,
+    up_pressed,
+    up_released,
+    down_pressed,
+    down_released,
+    select_pressed,
+    select_released,
+    start_pressed,
+    start_released,
+    button1_pressed,
+    button1_released,
+    button2_pressed,
+    button2_released,
+    /// virtual console events
+    turn_off
+};
+
+std::ostream& OM_DECLSPEC operator<<(std::ostream& stream, const event e);
+
+class engine;
+
+/// return not null on success
+engine* OM_DECLSPEC create_engine();
+void OM_DECLSPEC destroy_engine(engine* e);
+
+class OM_DECLSPEC color
+{
+public:
+    color() = default;
+    explicit color(std::uint32_t rgba_);
+    color(float r, float g, float b, float a);
+
+    float get_r() const;
+    float get_g() const;
+    float get_b() const;
+    float get_a() const;
+
+    void set_r(const float r);
+    void set_g(const float g);
+    void set_b(const float b);
+    void set_a(const float a);
+
+private:
+    std::uint32_t rgba = 0;
+};
+
+/// vertex with position only
+struct OM_DECLSPEC v0
+{
+    vec2 pos;
+};
+
+/// vertex with position and texture coordinate
+struct OM_DECLSPEC v1
+{
+    vec2  pos;
+    color c;
+};
+
+/// vertex position + color + texture coordinate
+struct OM_DECLSPEC v2
+{
+    vec2  pos;
+    vec2  uv;
+    color c;
+};
+
+/// triangle with positions only
+struct OM_DECLSPEC tri0
+{
+    tri0();
+    v0 v[3];
+};
+
+/// triangle with positions and color
+struct OM_DECLSPEC tri1
+{
+    tri1();
+    v1 v[3];
+};
+
+/// triangle with positions color and texture coordinate
+struct OM_DECLSPEC tri2
+{
+    tri2();
+    v2 v[3];
+};
+
+std::istream& OM_DECLSPEC operator>>(std::istream& is, mat2&);
+std::istream& OM_DECLSPEC operator>>(std::istream& is, vec2&);
+std::istream& OM_DECLSPEC operator>>(std::istream& is, color&);
+std::istream& OM_DECLSPEC operator>>(std::istream& is, v0&);
+std::istream& OM_DECLSPEC operator>>(std::istream& is, v1&);
+std::istream& OM_DECLSPEC operator>>(std::istream& is, v2&);
+std::istream& OM_DECLSPEC operator>>(std::istream& is, tri0&);
+std::istream& OM_DECLSPEC operator>>(std::istream& is, tri1&);
+std::istream& OM_DECLSPEC operator>>(std::istream& is, tri2&);
+
+class OM_DECLSPEC texture
+{
+public:
+    virtual ~texture();
+    virtual std::uint32_t get_width() const  = 0;
+    virtual std::uint32_t get_height() const = 0;
+};
+
+class OM_DECLSPEC engine
+{
+public:
+    virtual ~engine();
+    /// create main window
+    /// on success return empty string
+    virtual std::string initialize(std::string_view config) = 0;
+    /// return seconds from initialization
+    virtual float get_time_from_init() = 0;
+    /// pool event from input queue
+    /// return true if more events in queue
+    virtual bool read_input(event& e)                      = 0;
+    virtual texture* create_texture(std::string_view path) = 0;
+    virtual void destroy_texture(texture* t)               = 0;
+    virtual void render(const tri0&, const color&) = 0;
+    virtual void render(const tri1&) = 0;
+    virtual void render(const tri2&, texture*) = 0;
+    virtual void swap_buffers() = 0;
+    virtual void uninitialize() = 0;
+};
+
+} // end namespace om
---
+++ tests/06-1-matrix-and-vector/game.cxx
@@ -0,0 +1,158 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <string_view>
+
+#include "engine.hxx"
+
+om::v0 blend(const om::v0& vl, const om::v0& vr, const float a)
+{
+    om::v0 r;
+    r.pos.x = (1.0f - a) * vl.pos.x + a * vr.pos.x;
+    r.pos.y = (1.0f - a) * vl.pos.y + a * vr.pos.y;
+    return r;
+}
+
+om::tri0 blend(const om::tri0& tl, const om::tri0& tr, const float a)
+{
+    om::tri0 r;
+    r.v[0] = blend(tl.v[0], tr.v[0], a);
+    r.v[1] = blend(tl.v[1], tr.v[1], a);
+    r.v[2] = blend(tl.v[2], tr.v[2], a);
+    return r;
+}
+
+int main(int /*argc*/, char* /*argv*/ [])
+{
+    std::unique_ptr<om::engine, void (*)(om::engine*)> engine(
+        om::create_engine(), om::destroy_engine);
+
+    const std::string error = engine->initialize("");
+    if (!error.empty())
+    {
+        std::cerr << error << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    om::texture* texture = engine->create_texture("tank.png");
+    if (nullptr == texture)
+    {
+        std::cerr << "failed load texture\n";
+        return EXIT_FAILURE;
+    }
+
+    bool continue_loop  = true;
+    int  current_shader = 0;
+    while (continue_loop)
+    {
+        om::event event;
+
+        while (engine->read_input(event))
+        {
+            std::cout << event << std::endl;
+            switch (event)
+            {
+                case om::event::turn_off:
+                    continue_loop = false;
+                    break;
+                case om::event::button1_released:
+                    ++current_shader;
+                    if (current_shader > 2)
+                    {
+                        current_shader = 0;
+                    }
+                    break;
+                default:
+
+                    break;
+            }
+        }
+
+        if (current_shader == 0)
+        {
+            std::ifstream file("vert_pos.txt");
+            assert(!!file);
+
+            om::tri0 tr1;
+            om::tri0 tr2;
+            om::tri0 tr11;
+            om::tri0 tr22;
+
+            file >> tr1 >> tr2 >> tr11 >> tr22;
+
+            float time  = engine->get_time_from_init();
+            float alpha = std::sin(time);
+
+            om::tri0 t1 = blend(tr1, tr11, alpha);
+            om::tri0 t2 = blend(tr2, tr22, alpha);
+
+            engine->render(t1, om::color(1.f, 0.f, 0.f, 1.f));
+            engine->render(t2, om::color(0.f, 1.f, 0.f, 1.f));
+        }
+
+        if (current_shader == 1)
+        {
+            std::ifstream file("vert_pos_color.txt");
+            assert(!!file);
+
+            om::tri1 tr1;
+            om::tri1 tr2;
+
+            file >> tr1 >> tr2;
+
+            engine->render(tr1);
+            engine->render(tr2);
+        }
+
+        if (current_shader == 2)
+        {
+            std::ifstream file("vert_tex_color.txt");
+            assert(!!file);
+
+            om::tri2 tr1;
+            om::tri2 tr2;
+
+            file >> tr1 >> tr2;
+
+            float time = engine->get_time_from_init();
+            float s    = std::sin(time);
+            float c    = std::cos(time);
+
+            // animate one triangle texture coordinates
+            for (auto& v : tr1.v)
+            {
+                v.uv.x += c;
+                v.uv.y += s;
+            }
+
+            engine->render(tr1, texture);
+
+            om::mat2 aspect;
+            aspect.col0.x = 640.f / 480.f;
+            aspect.col0.y = 0.f;
+            aspect.col1.x = 0.f;
+            aspect.col1.y = 1.f;
+
+            om::mat2 m = aspect * om::mat2::rotation(time) *
+                         om::mat2::scale(std::sin(time));
+
+            for (auto& v : tr2.v)
+            {
+                v.pos = v.pos * m;
+            }
+
+            engine->render(tr2, texture);
+        }
+
+        engine->swap_buffers();
+    }
+
+    engine->uninitialize();
+
+    return EXIT_SUCCESS;
+}
