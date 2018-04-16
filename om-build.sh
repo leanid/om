cb4692f3aaa626f7760aa26cef03a81c6d95f8e1
Sergey Petrovskiy <favourite.weapon@gmail.com>
2018-04-16 22:16:09 +0300

    добавлен скрип сборки. протестирован на Win7x64.

---
+++ om-build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+declare -a arr=("base-devel" "git" "mingw-w64-x86_64-toolchain" "cmake" "mingw-w64-x86_64-SDL2")
+FREE=`df -k --output=avail "$PWD" | tail -n1`
+
+pacman -Suy --noconfirm 
+
+if [[ $FREE -lt 2350000 ]]; then
+     echo -e ${RED}"Not enough free space om current disk. Quitting"${NC}
+	 exit 2
+fi;
+
+for i in "${arr[@]}"
+do
+   echo -e ${YELLOW}Installing "$i" ${NC}
+   if pacman -Qi "$i" > /dev/null; then 
+   echo -e ${GREEN}"The package "$i" is already installed" ${NC}
+   else 
+   pacman -S --noconfirm --needed "$i"
+   fi 
+done
+ 
+if [ ! -d ~/om ]; then
+  echo -e ${RED}"Directory ~/om doesn't exists Quitting"${NC}
+  exit 3
+fi
+  
+cd ~/om 
+mkdir build 
+cd build 
+cmake ../tests -DCMAKE_SYSTEM_NAME="MSYS" 
+cmake --build . 
+exit 0 
