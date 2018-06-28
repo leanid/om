e368adb76d4ae59e12684b1f1dc4514749dd6300
codelimit <codelimit@192.168.3.8>
2018-06-28 16:52:16 +0300

    camelcase fix. refactoring.

--- engine/src/scanner_unit_test.cxx
+++ engine/src/scanner_unit_test.cxx
@@ -15,151 +15,117 @@ TEST_CASE("scanner initialization test")
     REQUIRE(report.total_folders == 8);
 }
 
-TEST_CASE("getFileSize test")
+TEST_CASE("get_file_size test")
 {
     om::scanner scnr;
-    SECTION("full  path")
+
+    SECTION("valid request")
     {
+        REQUIRE(om::get_file_size(&scnr, "game/game.cxx") == 100500);
+        REQUIRE(om::get_file_size(&scnr, "appveyor.yml") == 123);
+    }
+    SECTION("invalid request")
+    {
+        REQUIRE_THROWS_WITH(om::get_file_size(&scnr, "engine/src/scanner"),
+                            Contains("no name or/and extension present"));
+        REQUIRE_THROWS_WITH(om::get_file_size(&scnr, "readme"),
+                            Contains("no name or/and extension present"));
+        REQUIRE_THROWS_WITH(om::get_file_size(&scnr, ""),
+                            Contains("no name or/and extension present"));
+        REQUIRE_THROWS_WITH(om::get_file_size(&scnr, ".hxx"),
+                            Contains("no name or/and extension present"));
+        REQUIRE_THROWS_WITH(om::get_file_size(&scnr, "game/net.cxx"),
+                            Contains("not found"));
+        REQUIRE_THROWS_WITH(om::get_file_size(&scnr, "main.cxx"),
+                            Contains("not found"));
+    }
+}
+
+TEST_CASE("is_file_exists test")
+{
+    om::scanner scnr;
+    SECTION("valid request")
+    {
+        REQUIRE(om::is_file_exists(&scnr, "game/game.cxx") == true);
+        REQUIRE(om::is_file_exists(&scnr, "game/net.cxx") == false);
+        REQUIRE(om::is_file_exists(&scnr, "appveyor.yml") == true);
+        REQUIRE(om::is_file_exists(&scnr, "main.cxx") == false);
+    }
+    SECTION("invalid request")
+    {
+        REQUIRE_THROWS_WITH(om::is_file_exists(&scnr, "engine/src/scanner"),
+                            Contains("no name or/and extension present"));
+        REQUIRE_THROWS_WITH(om::is_file_exists(&scnr, "readme"),
+                            Contains("no name or/and extension present"));
+        REQUIRE_THROWS_WITH(om::is_file_exists(&scnr, ".md"),
+                            Contains("no name or/and extension present"));
+        REQUIRE_THROWS_WITH(om::is_file_exists(&scnr, ""),
+                            Contains("no name or/and extension present"));
+    }
+}
+
+TEST_CASE("get_all_files_with_extension test")
+{
+    om::scanner scnr;
+
+    SECTION("valid request")
+    {
+        om::file_list inf;
+        inf = om::get_all_files_with_extension(&scnr, "wtf", "engine/src");
+        REQUIRE(inf.size() == 0);
+        REQUIRE(inf.empty());
+        inf = om::get_all_files_with_extension(&scnr, "cxx", "engine/src");
+        REQUIRE(inf.size() == 2);
+        REQUIRE_FALSE(inf.empty());
+        inf = om::get_all_files_with_extension(&scnr, "yml", "");
+        REQUIRE(inf.size() == 2);
+        REQUIRE_FALSE(inf.empty());
+    }
+    SECTION("invalid request")
+    {
+        REQUIRE_THROWS_WITH(om::get_all_files_with_extension(
+                                &scnr, "cxx", "engine/src/readme.md"),
+                            Contains("path not found"));
+        REQUIRE_THROWS_WITH(om::get_all_files_with_extension(
+                                &scnr, "cxx", "engine/invalid_dir"),
+                            Contains("path not found"));
         REQUIRE_THROWS_WITH(
-            om::getFileSize(&scnr, "C:/Windows/System32/kernel32"),
+            om::get_all_files_with_extension(&scnr, "", "engine/src"),
             Contains("no extension present"));
-        REQUIRE_THROWS_WITH(
-            om::getFileSize(&scnr, "C:/Windows/System32/kernel100.dll"),
-            Contains("not found"));
-        REQUIRE(om::getFileSize(&scnr, "C:/Windows/System32/kernel32.dll") ==
-                100500);
-    }
-    SECTION("relative path from root directory")
-    {
-        REQUIRE_THROWS_WITH(om::getFileSize(&scnr, "engine/src/scanner"),
-                            Contains("no extension present"));
-        REQUIRE_THROWS_WITH(om::getFileSize(&scnr, "game/net.cxx"),
-                            Contains("not found"));
-        REQUIRE(om::getFileSize(&scnr, "game/game.cxx") == 100500);
-    }
-    SECTION("name only")
-    {
-        REQUIRE_THROWS_WITH(om::getFileSize(&scnr, "readme"),
-                            Contains("no extension present"));
-        REQUIRE_THROWS_WITH(om::getFileSize(&scnr, "main.cxx"),
-                            Contains("not found"));
-        REQUIRE(om::getFileSize(&scnr, "appveyor.yml") == 100500);
-    }
-    SECTION("empty string") { REQUIRE(om::getFileSize(&scnr, "") == false); }
-}
-
-TEST_CASE("isFileExists test")
-{
-    om::scanner scnr;
-    SECTION("full  path")
-    {
-        REQUIRE_THROWS_WITH(
-            om::isFileExists(&scnr, "C:/Windows/System32/kernel32"),
-            Contains("no name or/and extension present"));
-        REQUIRE_THROWS_WITH(om::isFileExists(&scnr, "C:/Windows/System32/.dll"),
-                            Contains("no name or/and extension present"));
-        REQUIRE(om::isFileExists(&scnr, "C:/Windows/System32/kernel32.dll") ==
-                true);
-        REQUIRE(om::isFileExists(&scnr, "C:/Windows/System32/kernel100.dll") ==
-                false);
-    }
-    SECTION("relative path from root directory")
-    {
-        REQUIRE_THROWS_WITH(om::isFileExists(&scnr, "engine/src/scanner"),
-                            Contains("no name or/and extension present"));
-        REQUIRE_THROWS_WITH(om::isFileExists(&scnr, "engine/src/.cxx"),
-                            Contains("no name or/and extension present"));
-        REQUIRE(om::isFileExists(&scnr, "game/game.cxx") == true);
-        REQUIRE(om::isFileExists(&scnr, "game/net.cxx") == false);
-    }
-    SECTION("name only")
-    {
-        REQUIRE_THROWS_WITH(om::isFileExists(&scnr, "readme"),
-                            Contains("no name or/and extension present"));
-        REQUIRE_THROWS_WITH(om::isFileExists(&scnr, ".md"),
-                            Contains("no name or/and extension present"));
-        REQUIRE(om::isFileExists(&scnr, "appveyor.yml") == true);
-        REQUIRE(om::isFileExists(&scnr, "main.cxx") == false);
-    }
-    SECTION("empty string") { REQUIRE(om::isFileExists(&scnr, "") == false); }
-}
-
-TEST_CASE("getAllFilesWithExtension test")
-{
-    om::scanner scnr;
-    SECTION("full  path")
-    {
-        REQUIRE_THROWS_WITH(
-            om::getAllFilesWithExtension(&scnr, "cxx",
-                                         "C:/Windows/System32/kernel32.dll"),
-            Contains("path not found"));
-        REQUIRE_THROWS_WITH(
-            om::getAllFilesWithExtension(&scnr, "wtf", "C:/Windows/System32"),
-            Contains("files not found"));
-        om::file_list inf =
-            om::getAllFilesWithExtension(&scnr, "dll", "C:/Windows/System32");
-        REQUIRE(inf.size() == 100500);
-        REQUIRE_FALSE(inf.empty());
-    }
-    SECTION("relative path from root directory")
-    {
-        REQUIRE_THROWS_WITH(
-            om::getAllFilesWithExtension(&scnr, "cxx", "engine/src/readme.md"),
-            Contains("path not found"));
-        REQUIRE_THROWS_WITH(
-            om::getAllFilesWithExtension(&scnr, "wtf", "engine/src"),
-            Contains("files not found"));
-        om::file_list inf =
-            om::getAllFilesWithExtension(&scnr, "cxx", "engine/src");
-        REQUIRE(inf.size() == 2);
-        REQUIRE_FALSE(inf.empty());
-    }
-    SECTION("empty string")
-    {
-        REQUIRE_THROWS_WITH(
-            om::getAllFilesWithExtension(&scnr, "", "engine/src"),
-            Contains("files not found"));
-        om::file_list inf = om::getAllFilesWithExtension(&scnr, "yml", "");
-        REQUIRE(inf.size() == 2);
-        REQUIRE_FALSE(inf.empty());
     }
 }
 
-TEST_CASE("getAllFilesWithName test")
+TEST_CASE("get_all_files_with_name test")
 {
     om::scanner scnr;
-    SECTION("full  path")
+
+    SECTION("valid request")
     {
-        REQUIRE_THROWS_WITH(
-            om::getAllFilesWithName(&scnr, "main",
-                                    "C:/Windows/System32/kernel32.dll"),
-            Contains("path not found"));
-        REQUIRE_THROWS_WITH(
-            om::getAllFilesWithName(&scnr, "wtf", "C:/Windows/System32"),
-            Contains("files not found"));
-        om::file_list inf =
-            om::getAllFilesWithName(&scnr, "winlogon", "C:/Windows/System32");
-        REQUIRE(inf.size() == 4);
-        REQUIRE_FALSE(inf.empty());
-    }
-    SECTION("relative path from root directory")
-    {
-        REQUIRE_THROWS_WITH(
-            om::getAllFilesWithName(&scnr, "readme", "engine/src/readme.md"),
-            Contains("path not found"));
-        REQUIRE_THROWS_WITH(om::getAllFilesWithName(&scnr, "wtf", "engine/src"),
-                            Contains("files not found"));
-        om::file_list inf =
-            om::getAllFilesWithName(&scnr, "readme", "engine/src");
+
+        om::file_list inf;
+        inf = om::get_all_files_with_name(&scnr, "wtf", "engine/src");
+        REQUIRE(inf.size() == 0);
+        REQUIRE(inf.empty());
+        inf = om::get_all_files_with_name(&scnr, "readme", "engine/src");
         REQUIRE(inf.size() == 1);
         REQUIRE_FALSE(inf.empty());
-    }
-    SECTION("empty string")
-    {
-        REQUIRE_THROWS_WITH(om::getAllFilesWithName(&scnr, "", "engine/src"),
-                            Contains("files not found"));
-        om::file_list inf = om::getAllFilesWithName(&scnr, "appveyor", "");
+        inf = om::get_all_files_with_name(&scnr, "", "engine/src");
+        REQUIRE(inf.size() == 8);
+        REQUIRE_FALSE(inf.empty());
+        inf = om::get_all_files_with_name(&scnr, "appveyor", "");
         REQUIRE(inf.size() == 1);
         REQUIRE_FALSE(inf.empty());
+        inf = om::get_all_files_with_name(&scnr, "", "");
+        REQUIRE(inf.size() == 6);
+        REQUIRE_FALSE(inf.empty());
+    }
+    SECTION("invalid request")
+    {
+        REQUIRE_THROWS_WITH(om::get_all_files_with_name(&scnr, "readme",
+                                                        "engine/src/readme.md"),
+                            Contains("path not found"));
+        REQUIRE_THROWS_WITH(
+            om::get_all_files_with_name(&scnr, "readme", "engine/invalid_dir"),
+            Contains("path not found"));
     }
 }
