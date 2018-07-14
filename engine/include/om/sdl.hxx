0ad3e75d44c573f83a799dab9f4e22f27a213d49
Леонид Чайка <leo@localhost.localdomain>
2018-07-15 00:03:55 +0300

    continue sdl wrapper

--- engine/include/om/sdl.hxx
+++ engine/include/om/sdl.hxx
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <any>
 #include <cstdint>
 #include <optional>
 #include <string_view>
@@ -41,6 +42,22 @@ struct display
 struct surface
 {
 };
+
+enum class hittest_result
+{
+    normal,
+    draggable,
+    resize_topleft,
+    resize_top,
+    resize_topright,
+    resize_right,
+    resize_bottomright,
+    resize_bottom,
+    resize_bottomleft,
+    resize_left
+};
+
+hittest_result hit_test(window& win, const point& p, std::any);
 
 struct window
 {
@@ -110,6 +127,49 @@ struct window
     bool get_gamma_ramp(std::optional<std::array<std::uint16_t, 256>*> red,
                         std::optional<std::array<std::uint16_t, 256>*> green,
                         std::optional<std::array<std::uint16_t, 256>*> blue);
+    bool set_hit_test(hit_test callback, std::any);
+};
+
+enum class gl_attribute
+{
+    gl_red_size,
+    gl_green_size,
+    gl_blue_size,
+    gl_alpha_size,
+    gl_buffer_size,
+    gl_doublebuffer,
+    gl_depth_size,
+    gl_stencil_size,
+    gl_accum_red_size,
+    gl_accum_green_size,
+    gl_accum_blue_size,
+    gl_accum_alpha_size,
+    gl_stereo,
+    gl_multisamplebuffers,
+    gl_multisamplesamples,
+    gl_accelerated_visual,
+    gl_retained_backing,
+    gl_context_major_version,
+    gl_context_minor_version,
+    gl_context_egl,
+    gl_context_flags,
+    gl_context_profile_mask,
+    gl_share_with_current_context,
+    gl_framebuffer_srgb_capable,
+    gl_context_release_behavior,
+    gl_context_reset_notification,
+    gl_context_no_error
+};
+
+struct gl_context
+{
+};
+
+enum class gl_swap_interval
+{
+    immediate,
+    synchronized,
+    late_swaps
 };
 
 struct video
@@ -125,8 +185,28 @@ struct video
     window create_window(std::string_view title, std::int32_t x, std::int32_t y,
                          std::int32_t w, std::int32_t h, window::flags flags);
     window create_window_from(const void* native_handle);
+    void   destroy_window(window&);
     window get_window_fromID(std::uint32_t id) const;
     std::optional<window> get_grabbed_window() const;
+    bool                  is_screen_saver_enabled() const;
+    void                  enable_screen_saver();
+    void                  disable_screen_saver();
+
+    bool         gl_load_library(std::string_view path);
+    void*        gl_get_proc_address(std::string_view proc);
+    void         gl_unload_library();
+    bool         gl_extension_supported(std::string_view extension);
+    void         gl_reset_attributes();
+    bool         gl_set_attribute(gl_attribute attr, std::int32_t value);
+    std::int32_t gl_get_attribute(gl_attribute attr);
+    gl_context   gl_create_context();
+    bool         gl_make_current(gl_context context);
+    window       gl_get_current_window();
+    gl_context   gl_get_current_context();
+    size         gl_get_drawable_size(window wnd) const;
+    bool         gl_set_swap_interval(gl_swap_interval);
+    void         gl_swap_window(window wnd);
+    void         gl_delete_context(gl_context);
 };
 
 struct sdlxx
