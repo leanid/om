8214e220537ef2c5b83b638777945aca8beef09e
leo <leanid.chaika@gmail.com>
2018-07-26 22:06:01 +0300

    add first step for graphics class prepare OpenGL ES 3.0 context on Fedora works easy.

---
+++ class-02-graphics-3d/00-1-prepare-linux-gles30/main.cxx
@@ -0,0 +1,95 @@
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengles2.h>
+
+int main(int /*argc*/, char* /*argv*/ [])
+{
+    using namespace std;
+
+    const int init_result = SDL_Init(SDL_INIT_EVERYTHING);
+    if (init_result != 0)
+
+    {
+        const char* err_message = SDL_GetError();
+        std::clog << "error: failed call SDL_Init: " << err_message << endl;
+        return -1;
+    }
+    else
+    {
+        std::atexit(SDL_Quit);
+    }
+
+    std::unique_ptr<SDL_Window, void (*)(SDL_Window*)> window(
+        SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED,
+                         SDL_WINDOWPOS_CENTERED, 640, 480, ::SDL_WINDOW_OPENGL),
+        SDL_DestroyWindow);
+
+    if (window == nullptr)
+    {
+        const char* err_message = SDL_GetError();
+        std::clog << "error: failed call SDL_CreateWindow: " << err_message
+                  << endl;
+        SDL_Quit();
+        return -1;
+    }
+
+    // we want ONLY OpenGLES 3.0 context or more
+    int r = SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
+                                SDL_GL_CONTEXT_PROFILE_ES);
+    SDL_assert_always(r == 0);
+    r = SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_assert_always(r == 0);
+    r = SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    SDL_assert_always(r == 0);
+
+    std::unique_ptr<void, void (*)(void*)> gl_context(
+        SDL_GL_CreateContext(window.get()), SDL_GL_DeleteContext);
+    if (nullptr == gl_context)
+    {
+        std::clog << "Failed to create OpenGL ES 3.0 context: "
+                  << SDL_GetError();
+        return -1;
+    }
+
+    int gl_major_ver = 0;
+    int result =
+        SDL_GL_GetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, &gl_major_ver);
+    SDL_assert_always(result == 0);
+    int gl_minor_ver = 0;
+    result = SDL_GL_GetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, &gl_minor_ver);
+    SDL_assert_always(result == 0);
+
+    std::clog << "Ask for OpenGL ES 3.0" << std::endl;
+    std::clog << "Receive OpenGL ES " << gl_major_ver << '.' << gl_minor_ver
+              << std::endl;
+
+    bool continue_loop = true;
+    while (continue_loop)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            if (SDL_QUIT == event.type)
+            {
+                continue_loop = false;
+                break;
+            }
+        }
+
+        float red   = 0.f;
+        float green = 1.f;
+        float blue  = 0.f;
+        float alpha = 0.f;
+
+        glClearColor(red, green, blue, alpha);
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        SDL_GL_SwapWindow(window.get());
+    }
+
+    return 0;
+}
