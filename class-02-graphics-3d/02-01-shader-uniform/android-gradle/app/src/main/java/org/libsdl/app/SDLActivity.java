89fbbe11a038dfab17fa273927e4d809596654b7
leo <leanid.chaika@gmail.com>
2018-08-05 18:56:20 +0300

    fix java part

--- class-02-graphics-3d/02-01-shader-uniform/android-gradle/app/src/main/java/org/libsdl/app/SDLActivity.java
+++ class-02-graphics-3d/02-01-shader-uniform/android-gradle/app/src/main/java/org/libsdl/app/SDLActivity.java
@@ -105,7 +105,7 @@ public class SDLActivity extends Activity {
             // "SDL2_mixer",
             // "SDL2_net",
             // "SDL2_ttf",
-            "00-6-prepare-all-gles30"
+            "02-01-shader-uniform"
         };
     }
 
