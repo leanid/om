625a60f251629173e60178084d6e8b8b3a9d3b7d
leo <leanid.chaika@gmail.com>
2018-09-15 16:03:04 +0300

    add asm main c++ file example

---
+++ class-00-basic-prog/01-hello-world/hack_asm_main.cxx
@@ -0,0 +1,24 @@
+extern "C" long syscall(long call_index, ...);
+
+constexpr long SYS_write = 1; // from <sys/syscall.h>
+constexpr long SYS_exit  = 60; // from <sys/syscall.h>
+
+int print(const char* message, int size)
+{
+    long exit_code = syscall(SYS_write, 1, message, size);
+    return static_cast<int>(exit_code);
+}
+
+int main(int /*argc*/, char* /*argv*/ [])
+{
+    return print("hello world\n", 12);
+}
+
+extern "C"
+{
+    void _start()
+    {
+        int result_code = main(0, nullptr);
+        syscall(SYS_exit, result_code);
+    }
+}
