0e3311380377085435f3b47406b51496b1a8c39c
Леонид Чайка <leanid.chaika@gmail.com>
2019-02-12 07:42:49 +0000

    main.cxx отредактирован онлайн на Bitbucket

--- class-00-basic-prog/04-new-delete-overload/main.cxx
+++ class-00-basic-prog/04-new-delete-overload/main.cxx
@@ -1,6 +1,8 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <sstream>
+#include <cstring>
 
 void* operator new(size_t n) noexcept(false)
 {
@@ -15,19 +17,39 @@ void operator delete(void* p) noexcept(true)
     ::free(p);
 }
 
+void operator delete(void* p, std::size_t n) noexcept(true)
+{
+    std::cout << "delete " << p << " size " << n << '\n';
+    ::free(p);
+}
+
 int main(int argc, char* argv[])
 {
-    std::cout << "next code will not allocate any memory\n";
-    std::cout << "hello world " << argc << ' ' << 3.14159 << ' '
+    std::cout << "-main-start------------------------------------------------------\n" << std::flush;
+    {
+        std::cout << "next line will not allocate any memory\n" << std::flush;
+        std::cout << "hello world " << argc << ' ' << 3.14159 << ' '
               << argv[argc - 1] << '\n';
-    std::string str("next line allocate memory for string some long string not "
+        std::string str("next line allocate memory for string some long string not "
                     "fit into 16 bytes optimization");
-    std::cout << "length of str is " << std::size(str) << '\n';
-    std::vector<char> vec;
-    std::cout << "prepare for 16Mb allocation!" << std::endl;
-    vec.resize(1024 * 1024 * 16); // 16 Mb allocation!!!
-    std::cout << "no more allocations\n";
-    std::cout << str << '\n';
-    std::cout << std::flush;
+        std::cout << "length of str is " << std::size(str) << '\n';
+        std::vector<char> vec;
+        std::cout << "prepare for 16Mb allocation!" << std::endl;
+        vec.resize(1024 * 1024 * 16); // 16 Mb allocation!!!
+        std::cout << "no more allocations\n";
+        std::cout << str << '\n';
+        std::cout << std::flush;
+    }
+    {
+        std::cout << "-play-with-string-stream-------------------------------------------------\n" << std::flush;
+        std::stringstream ss;
+        ss << "hello world\n";
+        const char* s = "asdfasdfasdfasfdasdfasfdasdfasfdasdfasfdsdfasdfasfdafdsfasfdasdfasdfasfdfsadfafsdasfdasfdafdsasfdasfd";
+        const size_t len = strlen(s);
+        ss << s << " " << len << '\n';
+        std::cout << ss.str() << std::flush;
+        
+    }
+    std::cout << "-main-finish------------------------------------------------------\n" << std::flush;
     return 0;
 }
