d9cd83859d5cc4afeaf06e3ab8e0b09a082a5d89
leo <leanid.chaika@gmail.com>
2019-05-07 00:15:51 +0300

    prepare template to next step - fps camera tutorial

---
+++ class-02-graphics-3d/06-01-camera/res/runtime.properties.hxx
@@ -0,0 +1,21 @@
+#include <glm/vec3.hpp>
+#include <string>
+
+// you can use this header file directly in your build
+// also you can update during runtime this values with
+// properties_reader.hxx
+
+bool        enable_depth  = true;
+std::string title         = "1-triangles, 2-lines, 3-line-strip, 4-line-loop";
+float       z_near        = 0.1f;
+float       z_far         = 100.f;
+float       fovy          = 45.f;
+float       screen_width  = 640.f;
+float       screen_height = 480.f;
+float       aspect        = screen_width / screen_height;
+float       angle         = 60.f;
+glm::vec3   rotate_axis   = { 1.f, 0.f, 0.f };
+float       angle_per_sec = 10.f;
+glm::vec3   move_camera   = { 0.f, 0.f, -5.f };
+bool        use_cube      = true;
+bool        multi_cube    = true;
