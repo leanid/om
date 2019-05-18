4ed7b0d728207c91802022f7b436c17d7d1b689e
leo <leanid.chaika@gmail.com>
2019-05-18 18:00:33 +0300

    start next tutorial basic lighting

---
+++ class-02-graphics-3d/08-01-basic-lighting/res/runtime.properties.hxx
@@ -0,0 +1,14 @@
+#include <glm/vec3.hpp>
+#include <string>
+
+// you can use this header file directly in your build
+// also you can update during runtime this values with
+// properties_reader.hxx
+
+std::string title         = "1-triangles, 2-lines, 3-line-strip, 4-line-loop";
+float       z_near        = 0.1f;
+float       z_far         = 100.f;
+float       fovy          = 45.f;
+float       screen_width  = 640.f;
+float       screen_height = 480.f;
+float       aspect        = screen_width / screen_height;
