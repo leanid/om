0ce40a192916b603b95ea2f2a2a26718c678169a
leanid chaika <leanid.chaika@gmail.com>
2019-07-02 00:02:11 +0300

    лучше путь будет чесно брать имя пользователя, простым способом

--- class-01-basic-game-dev/01-2-hello-lib/hello_bin/src/main.cxx
+++ class-01-basic-game-dev/01-2-hello-lib/hello_bin/src/main.cxx
@@ -2,9 +2,14 @@
 
 #include <hello_lib.hxx>
 
-int main(int /*argc*/, char* /*argv*/ [])
+int main(int /*argc*/, char* /*argv*/[], char* /*argv_env*/[])
 {
-    bool is_good = greetings("Leanid");
+    const char*      user_env_var = std::getenv("USER");
+    std::string_view user         = user_env_var != nullptr
+                                ? user_env_var
+                                : "USER - environment variable not found";
+
+    bool is_good = greetings(user);
 
     int result = is_good ? EXIT_SUCCESS : EXIT_FAILURE;
     return result;
