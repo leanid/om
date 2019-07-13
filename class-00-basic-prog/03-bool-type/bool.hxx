a515b0007a9b997bca31d16180b32652d4c95d78
leanid chaika <leanid.chaika@gmail.com>
2019-07-14 00:30:02 +0300

    continue bool example

--- class-00-basic-prog/03-bool-type/bool.hxx
+++ class-00-basic-prog/03-bool-type/bool.hxx
@@ -1,4 +1,39 @@
-#ifndef BOOL_HXX
-#define BOOL_HXX
+#pragma once
 
-#endif // BOOL_HXX
+#include <cstddef>
+
+namespace om
+{
+/// from standard:
+/// bool - type, capable of holding one of the two values: true or false.
+/// The value of sizeof(bool) is implementation defined and might differ from 1.
+class bool_
+{
+public:
+    bool_()                   = default;
+    bool_(const bool_& other) = default;
+    bool_(bool_&&)            = default;
+    bool_& operator=(const bool_&) = default;
+    bool_& operator=(bool_&&) = default;
+
+    friend bool_ operator&&(const bool_& l, const bool_& r);
+    friend bool_ operator||(const bool_& l, const bool_& r);
+    bool_        operator!();
+};
+
+extern const bool_ true_;
+extern const bool_ false_;
+
+/// operation | first value | second value | result
+/// -----------------------------------------------
+///     &&    |    true     |   true       | true
+///     &&    |    true     |   false      | false
+///     &&    |    false    |   true       | false
+///     &&    |    false    |   false      | false
+/// -----------------------------------------------
+///     ||    |    true     |   true       | true
+///     ||    |    true     |   false      | true
+///     ||    |    false    |   true       | true
+///     ||    |    false    |   false      | false
+
+} // end namespace om
