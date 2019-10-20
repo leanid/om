d1caa277af60ab5b1efbac7af94b16445bf6571e
Leanid Chayka <leanid.chaika@gmail.com>
2019-10-21 00:56:05 +0300

    simple static and dynamic libs

--- class-01-basic-game-dev/01-2-hello-lib/hello_lib/include/hello_lib.hxx
+++ class-01-basic-game-dev/01-2-hello-lib/hello_lib/include/hello_lib.hxx
@@ -1,4 +1,4 @@
 #include <string_view>
 
 /// print to stdout "hello, {user_name}" and return true on success
-extern bool greetings(std::string_view user_name);
+bool greetings(std::string_view user_name);
