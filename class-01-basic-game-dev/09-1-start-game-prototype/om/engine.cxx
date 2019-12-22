6e4d29672ef59589ff49ae8caefcb1e7659da670
Leanid Chayka <leanid.chaika@gmail.com>
2019-12-22 12:13:06 +0300

    need to fix crush on exit

--- class-01-basic-game-dev/09-1-start-game-prototype/om/engine.cxx
+++ class-01-basic-game-dev/09-1-start-game-prototype/om/engine.cxx
@@ -236,27 +236,30 @@ static std::size_t get_sound_format_size(uint16_t format_value)
 }
 
 // Yes! Global variables! /////////////////////////////////////////////////////
-std::ostream&     log(std::cout);
-std::atomic<bool> already_exist = false;
-SDL_Window*       window        = nullptr;
-SDL_GLContext     gl_context    = nullptr;
+std::ostream&            log(std::cout);
+static std::atomic<bool> already_exist = false;
+static SDL_Window*       window        = nullptr;
+static SDL_GLContext     gl_context    = nullptr;
 
 class shader_gl_es20;
 
-shader_gl_es20* shader00 = nullptr;
-shader_gl_es20* shader01 = nullptr;
-shader_gl_es20* shader02 = nullptr;
-shader_gl_es20* shader03 = nullptr;
+static shader_gl_es20* shader00 = nullptr;
+static shader_gl_es20* shader01 = nullptr;
+static shader_gl_es20* shader02 = nullptr;
+static shader_gl_es20* shader03 = nullptr;
 
-SDL_AudioDeviceID audio_device;
-SDL_AudioSpec     audio_device_spec;
+static SDL_AudioDeviceID audio_device;
+static SDL_AudioSpec     audio_device_spec;
 
 class sound_buffer_impl;
 
-std::vector<sound_buffer_impl*> sounds;
+static std::vector<sound_buffer_impl*> sounds;
 
-bool developer_mode = true;
-bool reload_game    = false;
+static bool developer_mode     = true;
+static bool reload_game        = false;
+static bool continue_game_loop = true;
+
+static int32_t exit_code = EXIT_FAILURE;
 // no more Globals ////////////////////////////////////////////////////////////
 
 class sound_buffer_impl final : public sound
@@ -394,9 +397,9 @@ public:
     {
         GLboolean is_texture = glIsTexture(tex_handl);
         SDL_assert(is_texture);
-        OM_GL_CHECK();
+        OM_GL_CHECK()
         glBindTexture(GL_TEXTURE_2D, tex_handl);
-        OM_GL_CHECK();
+        OM_GL_CHECK()
     }
 
     std::uint32_t get_width() const final { return width; }
@@ -432,7 +435,7 @@ public:
     void use() const
     {
         glUseProgram(program_id);
-        OM_GL_CHECK();
+        OM_GL_CHECK()
     }
 
     void set_uniform(std::string_view       uniform_name,
@@ -441,7 +444,7 @@ public:
         assert(texture != nullptr);
         const int location =
             glGetUniformLocation(program_id, uniform_name.data());
-        OM_GL_CHECK();
+        OM_GL_CHECK()
         if (location == -1)
         {
             std::cerr << "can't get uniform location from shader\n";
@@ -449,20 +452,20 @@ public:
         }
         unsigned int texture_unit = 0;
         glActiveTexture_(GL_TEXTURE0 + texture_unit);
-        OM_GL_CHECK();
+        OM_GL_CHECK()
 
         texture->bind();
 
         // http://www.khronos.org/opengles/sdk/docs/man/xhtml/glUniform.xml
         glUniform1i(location, static_cast<int>(0 + texture_unit));
-        OM_GL_CHECK();
+        OM_GL_CHECK()
     }
 
     void set_uniform(std::string_view uniform_name, const color& c)
     {
         const int location =
             glGetUniformLocation(program_id, uniform_name.data());
-        OM_GL_CHECK();
+        OM_GL_CHECK()
         if (location == -1)
         {
             std::cerr << "can't get uniform location from shader\n";
@@ -470,14 +473,14 @@ public:
         }
         float values[4] = { c.get_r(), c.get_g(), c.get_b(), c.get_a() };
         glUniform4fv(location, 1, &values[0]);
-        OM_GL_CHECK();
+        OM_GL_CHECK()
     }
 
     void set_uniform(std::string_view uniform_name, const matrix& m)
     {
         const int location =
             glGetUniformLocation(program_id, uniform_name.data());
-        OM_GL_CHECK();
+        OM_GL_CHECK()
         if (location == -1)
         {
             std::cerr << "can't get uniform location from shader\n";
@@ -490,35 +493,35 @@ public:
                             0.f,      0.f,       1.f };
         // clang-format on
         glUniformMatrix3fv(location, 1, GL_FALSE, &values[0]);
-        OM_GL_CHECK();
+        OM_GL_CHECK()
     }
 
 private:
     GLuint compile_shader(GLenum shader_type, std::string_view src)
     {
         GLuint shader_id = glCreateShader(shader_type);
-        OM_GL_CHECK();
+        OM_GL_CHECK()
         std::string_view vertex_shader_src = src;
         const char*      source            = vertex_shader_src.data();
         glShaderSource(shader_id, 1, &source, nullptr);
-        OM_GL_CHECK();
+        OM_GL_CHECK()
 
         glCompileShader(shader_id);
-        OM_GL_CHECK();
+        OM_GL_CHECK()
 
         GLint compiled_status = 0;
         glGetShaderiv(shader_id, GL_COMPILE_STATUS, &compiled_status);
-        OM_GL_CHECK();
+        OM_GL_CHECK()
         if (compiled_status == 0)
         {
             GLint info_len = 0;
             glGetShaderiv(shader_id, GL_INFO_LOG_LENGTH, &info_len);
-            OM_GL_CHECK();
+            OM_GL_CHECK()
             std::vector<char> info_chars(static_cast<size_t>(info_len));
             glGetShaderInfoLog(shader_id, info_len, NULL, info_chars.data());
-            OM_GL_CHECK();
+            OM_GL_CHECK()
             glDeleteShader(shader_id);
-            OM_GL_CHECK();
+            OM_GL_CHECK()
 
             std::string shader_type_name =
                 shader_type == GL_VERTEX_SHADER ? "vertex" : "fragment";
@@ -533,7 +536,7 @@ private:
         const std::vector<std::tuple<GLuint, const GLchar*>>& attributes)
     {
         GLuint program_id_ = glCreateProgram();
-        OM_GL_CHECK();
+        OM_GL_CHECK()
         if (0 == program_id_)
         {
             std::cerr << "failed to create gl program";
@@ -541,9 +544,9 @@ private:
         }
 
         glAttachShader(program_id_, vert_shader);
-        OM_GL_CHECK();
+        OM_GL_CHECK()
         glAttachShader(program_id_, frag_shader);
-        OM_GL_CHECK();
+        OM_GL_CHECK()
 
         // bind attribute location
         for (const auto& attr : attributes)
@@ -551,27 +554,27 @@ private:
             GLuint        loc  = std::get<0>(attr);
             const GLchar* name = std::get<1>(attr);
             glBindAttribLocation(program_id_, loc, name);
-            OM_GL_CHECK();
+            OM_GL_CHECK()
         }
 
         // link program after binding attribute locations
         glLinkProgram(program_id_);
-        OM_GL_CHECK();
+        OM_GL_CHECK()
         // Check the link status
         GLint linked_status = 0;
         glGetProgramiv(program_id_, GL_LINK_STATUS, &linked_status);
-        OM_GL_CHECK();
+        OM_GL_CHECK()
         if (linked_status == 0)
         {
             GLint infoLen = 0;
             glGetProgramiv(program_id_, GL_INFO_LOG_LENGTH, &infoLen);
-            OM_GL_CHECK();
+            OM_GL_CHECK()
             std::vector<char> infoLog(static_cast<size_t>(infoLen));
             glGetProgramInfoLog(program_id_, infoLen, NULL, infoLog.data());
-            OM_GL_CHECK();
+            OM_GL_CHECK()
             std::cerr << "Error linking program:\n" << infoLog.data();
             glDeleteProgram(program_id_);
-            OM_GL_CHECK();
+            OM_GL_CHECK()
             return 0;
         }
         return program_id_;
@@ -811,31 +814,31 @@ void render(const primitives primitive_type, const vbo& buff,
     const vertex* t = buff.data();
     // positions
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(vertex), &t->pos);
-    OM_GL_CHECK();
+    OM_GL_CHECK()
     glEnableVertexAttribArray(0);
-    OM_GL_CHECK();
+    OM_GL_CHECK()
     // colors
     glVertexAttribPointer(1, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(vertex),
                           &t->c);
-    OM_GL_CHECK();
+    OM_GL_CHECK()
     glEnableVertexAttribArray(1);
-    OM_GL_CHECK();
+    OM_GL_CHECK()
 
     // texture coordinates
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(vertex), &t->uv);
-    OM_GL_CHECK();
+    OM_GL_CHECK()
     glEnableVertexAttribArray(2);
-    OM_GL_CHECK();
+    OM_GL_CHECK()
 
     GLsizei num_of_vertexes = static_cast<GLsizei>(buff.size());
     GLenum  priv_type = primitive_types[static_cast<uint32_t>(primitive_type)];
     glDrawArrays(priv_type, 0, num_of_vertexes);
-    OM_GL_CHECK();
+    OM_GL_CHECK()
 
     glDisableVertexAttribArray(1);
-    OM_GL_CHECK();
+    OM_GL_CHECK()
     glDisableVertexAttribArray(2);
-    OM_GL_CHECK();
+    OM_GL_CHECK()
 }
 
 static void swap_buffers()
@@ -843,12 +846,13 @@ static void swap_buffers()
     SDL_GL_SwapWindow(window);
 
     glClear(GL_COLOR_BUFFER_BIT);
-    OM_GL_CHECK();
+    OM_GL_CHECK()
 }
 
 void exit(int return_code)
 {
-    std::exit(return_code);
+    continue_game_loop = false;
+    exit_code          = return_code;
 }
 
 static void audio_callback(void*, uint8_t*, int);
@@ -1088,15 +1092,15 @@ static void initialize_internal(std::string_view   title,
         shader03->use();
 
         glEnable(GL_BLEND);
-        OM_GL_CHECK();
+        OM_GL_CHECK()
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-        OM_GL_CHECK();
+        OM_GL_CHECK()
 
         glClearColor(0.f, 0.0, 0.f, 0.0f);
-        OM_GL_CHECK();
+        OM_GL_CHECK()
 
         glViewport(0, 0, window_size_w, window_size_h);
-        OM_GL_CHECK();
+        OM_GL_CHECK()
 
         // initialize audio
         audio_device_spec.freq     = 48000;
@@ -1283,9 +1287,9 @@ texture_gl_es20::texture_gl_es20(std::string_view path)
     }
 
     glGenTextures(1, &tex_handl);
-    OM_GL_CHECK();
+    OM_GL_CHECK()
     glBindTexture(GL_TEXTURE_2D, tex_handl);
-    OM_GL_CHECK();
+    OM_GL_CHECK()
 
     GLint   mipmap_level = 0;
     GLint   border       = 0;
@@ -1293,18 +1297,18 @@ texture_gl_es20::texture_gl_es20(std::string_view path)
     GLsizei height       = static_cast<GLsizei>(img.height);
     glTexImage2D(GL_TEXTURE_2D, mipmap_level, GL_RGBA, width, height, border,
                  GL_RGBA, GL_UNSIGNED_BYTE, &img.raw_image[0]);
-    OM_GL_CHECK();
+    OM_GL_CHECK()
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    OM_GL_CHECK();
+    OM_GL_CHECK()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    OM_GL_CHECK();
+    OM_GL_CHECK()
 }
 
 texture_gl_es20::~texture_gl_es20()
 {
     glDeleteTextures(1, &tex_handl);
-    OM_GL_CHECK();
+    OM_GL_CHECK()
 }
 
 void audio_callback(void*, uint8_t* stream, int stream_size)
@@ -1376,7 +1380,8 @@ int initialize_and_start_main_loop()
 
     std::vector<const char*> lib_names{
         { "libgame-09-1.dll", "libgame-09-1.so", "game-09-1.so",
-          "./build/Debug/libgame-09-1.so", "./build/Debug/libgame-09-1.dll" }
+          "./build/Debug/libgame-09-1.so", "./build/Debug/libgame-09-1.dll",
+          "./build-Debug/libgame-09-1.so" }
     };
 
     void* so_handle   = nullptr;
@@ -1447,7 +1452,7 @@ start_game_again:
 
     game->on_initialize();
 
-    while (true)
+    while (om::continue_game_loop)
     {
         time_point end_last_frame = timer.now();
         om::event  event;
@@ -1478,10 +1483,10 @@ start_game_again:
         }
     }
 
-    return EXIT_SUCCESS;
+    return om::exit_code;
 }
 
-int main(int /*argc*/, char* /*argv*/ [])
+int main(int /*argc*/, char* /*argv*/[])
 {
     try
     {
