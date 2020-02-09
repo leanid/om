7e42638a4bc8ab53855c16b48ea6ed594d99c028
Leanid Chayka <leanid.chaika@gmail.com>
2020-02-09 13:38:58 +0300

    minimal concepts example (not working for g++9.2)

---
+++ class-00-basic-prog/09-concepts/main.cxx
@@ -0,0 +1,31 @@
+#include <concepts>
+#include <cstdlib>
+#include <iostream>
+
+// Declaration of the concept "Hashable", which is satisfied by
+// any type T such that for values a of type T,
+// the expression std::hash<T>{}(a) compiles and its result is convertible to
+// std::size_t
+template <typename T>
+concept Hashable = requires(T a)
+{
+    {
+        std::hash<T>{}(a)
+    }
+    ->std::convertible_to<std::size_t>;
+};
+
+void some_func();
+
+int main(int argc, char* argv[])
+{
+    using namespace std;
+
+    return cout.fail() ? EXIT_FAILURE : EXIT_SUCCESS;
+}
+
+void some_func()
+{
+    using namespace std;
+    cout << "just some function call" << endl;
+}
