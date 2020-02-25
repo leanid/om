2a8f50be266543060fa03b9bb06802a138d8f8b6
Leanid <leanid.chaika@gmail.com>
2020-02-25 14:49:38 +0300

    refraction tutorial works! nice!

---
+++ class-02-graphics-3d/21-04-cubemap-refraction/fps_camera.hxx
@@ -0,0 +1,222 @@
+#pragma once
+
+#include <algorithm>
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/vec3.hpp>
+
+class fps_camera
+{
+public:
+    enum class step
+    {
+        forward,
+        backward,
+        left,
+        right
+    };
+    ///
+    /// \brief fps_camera
+    /// \param a_position coordinate in world space
+    /// \param a_direction vector from camera position to object
+    /// \param a_up vector in world space
+    ///
+    fps_camera(glm::vec3 a_position, glm::vec3 a_direction, glm::vec3 a_up);
+    fps_camera()                  = default;
+    fps_camera(const fps_camera&) = default;
+    fps_camera& operator=(const fps_camera&) = default;
+
+    glm::mat4 view_matrix() const;
+    glm::mat4 projection_matrix() const;
+
+    void move(const step direction, const float delta_time);
+    ///
+    /// \brief rotate
+    /// \param delta_yaw in degrees
+    /// \param delta_pitch in degrees
+    ///
+    void rotate(const float delta_yaw, const float delta_pitch);
+
+    void zoom(const float zoom);
+
+    void move_using_keyboard_wasd(const float delta_time);
+
+    glm::vec3 position() const;
+    void      position(const glm::vec3& value);
+
+    float fovy() const;
+    void  fovy(const float);
+
+    float aspect() const;
+    void  aspect(const float);
+
+    float z_near() const;
+    void  z_near(const float);
+
+    float z_far() const;
+    void  z_far(const float);
+
+    ///
+    /// \brief direction where camera is looking at
+    /// \return normalized verctor
+    glm::vec3 direction() const;
+
+private:
+    // Calculates the front vector from the Camera's (updated) Euler Angles
+    void update_camera_vectors()
+    {
+        using namespace std;
+        using namespace glm;
+        // Calculate the new Front vector
+        vec3  front_;
+        float yaw_rad   = radians(yaw);
+        float pitch_rad = radians(pitch);
+        front_.x        = sin(yaw_rad);
+        front_.y        = sin(pitch_rad);
+        // in OpenGL camera look backward to z axis (default NDC coord)
+        front_.z = -1.f * cos(pitch_rad) * cos(yaw_rad);
+
+        front = normalize(front_);
+        // Also re-calculate the Right and Up vector
+        right = normalize(cross(
+            front, world_up)); // Normalize the vectors, because their length
+                               // gets closer to 0 the more you look up or down
+                               // which results in slower movement.
+        up = normalize(cross(right, front));
+    }
+
+    // camera attributes
+    glm::vec3 position_;
+    glm::vec3 front{ 0.f, 0.f, -1.f };
+    glm::vec3 up{ 0.f, 1.f, 0.f };
+    glm::vec3 right;
+    glm::vec3 world_up{ 0.f, 1.f, 0.f };
+    // projection attributes
+    float fovy_{ 45.f };
+    float aspect_{ 1.f };
+    float z_near_{ 0.1f };
+    float z_far_{ 100.f };
+    // Euler angles
+    float yaw{ 0.f };   /// in degrees
+    float pitch{ 0.f }; /// in degrees
+    // camera options
+    float movement_speed{ 3.0f };
+};
+
+inline glm::mat4 fps_camera::view_matrix() const
+{
+    return glm::lookAt(position_, position_ + front, up);
+}
+
+inline glm::mat4 fps_camera::projection_matrix() const
+{
+    return glm::perspective(glm::radians(fovy_), aspect_, z_near_, z_far_);
+}
+
+inline fps_camera::fps_camera(glm::vec3 a_position, glm::vec3 a_direction,
+                              glm::vec3 a_up)
+{
+    a_direction = glm::normalize(a_direction);
+    // In OpenGL camera
+    // direction vector pointing from camera position
+    // to oposite side from object.
+    // a_direction = -1.f;
+    // if direction {0, 0, -1} expect yaw == 0 degrees
+    float yaw_rad = std::asin(a_direction.x);
+    yaw           = glm::degrees(yaw_rad);
+    // if direction {0, 0, -1} expect pitch == 0 degrees
+    float pitch_rad = std::asin(a_direction.y);
+    pitch           = glm::degrees(pitch_rad);
+
+    position_ = a_position;
+    world_up  = glm::normalize(a_up);
+
+    update_camera_vectors();
+}
+
+inline void fps_camera::move(const step direction, const float delta_time)
+{
+    const float velocity = movement_speed * delta_time;
+
+    switch (direction)
+    {
+        case step::forward:
+            position_ += front * velocity;
+            break;
+        case step::backward:
+            position_ -= front * velocity;
+            break;
+        case step::left:
+            position_ -= right * velocity;
+            break;
+        case step::right:
+            position_ += right * velocity;
+            break;
+    }
+}
+
+inline void fps_camera::rotate(const float delta_yaw, const float delta_pitch)
+{
+    yaw += delta_yaw;
+    pitch += delta_pitch;
+
+    std::clamp(pitch, -89.f, 89.f);
+
+    update_camera_vectors();
+}
+
+inline void fps_camera::zoom(const float zoom)
+{
+    fovy_ += zoom;
+
+    std::clamp(fovy_, 1.f, 45.f);
+}
+
+inline float fps_camera::fovy() const
+{
+    return fovy_;
+}
+inline void fps_camera::fovy(const float v)
+{
+    fovy_ = v;
+    std::clamp(fovy_, 1.f, 45.f);
+}
+
+inline float fps_camera::aspect() const
+{
+    return aspect_;
+}
+
+inline void fps_camera::aspect(const float v)
+{
+    aspect_ = v;
+    std::clamp(aspect_, 0.1f, 10.f);
+}
+
+inline float fps_camera::z_near() const
+{
+    return z_near_;
+}
+
+inline void fps_camera::z_near(const float v)
+{
+    z_near_ = v;
+    std::clamp(z_near_, 0.01f, 10000.f);
+}
+
+inline float fps_camera::z_far() const
+{
+    return z_far_;
+}
+
+inline void fps_camera::z_far(const float v)
+{
+    z_far_ = v;
+    std::clamp(z_far_, z_near_, 10000.f);
+}
+
+inline glm::vec3 fps_camera::direction() const
+{
+    return glm::normalize(front);
+}
---
+++ class-02-graphics-3d/21-04-cubemap-refraction/gles30_shader.cxx
@@ -0,0 +1,258 @@
+#include "gles30_shader.hxx"
+
+#include <algorithm>
+#include <cassert>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <utility>
+
+#include "gles30_texture.hxx"
+#include "opengles30.hxx"
+
+namespace gles30
+{
+void shader::create(std::string_view vertex_shader_src,
+                    std::string_view fragment_shader_src)
+{
+    // create OpenGL object id for vertex shader object
+    uint32_t vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+
+    // load vertex shader source code into vertex_shader
+    const GLchar* array_of_pointers_to_strings_with_src[1];
+    array_of_pointers_to_strings_with_src[0] = vertex_shader_src.data();
+
+    GLint array_of_string_lengths[1];
+    array_of_string_lengths[0] = static_cast<GLint>(vertex_shader_src.size());
+
+    glShaderSource(vertex_shader, 1, array_of_pointers_to_strings_with_src,
+                   array_of_string_lengths);
+
+    // compile vertex shader
+    glCompileShader(vertex_shader);
+
+    // check compilation status of our shader
+    int  success;
+    char info_log[1024] = { 0 };
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+
+    if (0 == success)
+    {
+        glGetShaderInfoLog(vertex_shader, sizeof(info_log), nullptr, info_log);
+
+        std::stringstream ss;
+        ss << "error: in vertex shader: " << info_log << std::endl;
+        throw std::runtime_error(ss.str());
+    }
+
+    // generate new id for shader object
+    uint32_t fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    // load fragment shader source code
+    array_of_pointers_to_strings_with_src[0] = fragment_shader_src.data();
+    array_of_string_lengths[0] = static_cast<GLint>(fragment_shader_src.size());
+    glShaderSource(fragment_shader, 1, array_of_pointers_to_strings_with_src,
+                   array_of_string_lengths);
+
+    glCompileShader(fragment_shader);
+
+    // check compilation status of our shader
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+
+    if (0 == success)
+    {
+        glGetShaderInfoLog(fragment_shader, sizeof(info_log), nullptr,
+                           info_log);
+
+        std::stringstream ss;
+        ss << "error: in fragment shader: " << info_log << std::endl;
+        throw std::runtime_error(ss.str());
+    }
+
+    // create complete shader program and reseive id (vertex + geometry +
+    // fragment) geometry shader - will have default value
+    program_id = glCreateProgram();
+
+    glAttachShader(program_id, vertex_shader);
+
+    glAttachShader(program_id, fragment_shader);
+
+    // no link program like in c/c++ object files
+    glLinkProgram(program_id);
+
+    glGetProgramiv(program_id, GL_LINK_STATUS, &success);
+
+    if (0 == success)
+    {
+        glGetProgramInfoLog(program_id, sizeof(info_log), nullptr, info_log);
+
+        std::stringstream ss;
+        ss << "error: linking: " << info_log << std::endl;
+        throw std::runtime_error(ss.str());
+    }
+
+    // after linking shader program we don't need object parts of it
+    // so we can free OpenGL memory and delete vertex and fragment parts
+    glDeleteShader(vertex_shader);
+
+    glDeleteShader(fragment_shader);
+}
+
+shader::shader(
+    const std::filesystem::path& vertex_shader_path,
+    const std::filesystem::path& fragment_shader_path) noexcept(false)
+    : program_id(0)
+{
+    std::ifstream vertex_stream;
+    std::ifstream fragmen_stream;
+    vertex_stream.exceptions(std::ifstream::badbit | std::ifstream::failbit);
+    fragmen_stream.exceptions(std::ifstream::badbit | std::ifstream::failbit);
+
+    try
+    {
+        try
+        {
+            vertex_stream.open(vertex_shader_path);
+        }
+        catch (...)
+        {
+            std::cerr << "failed to open: " << vertex_shader_path << std::endl;
+            throw;
+        }
+
+        try
+        {
+            fragmen_stream.open(fragment_shader_path);
+        }
+        catch (...)
+        {
+            std::cerr << "faild to open: " << fragment_shader_path << std::endl;
+            throw;
+        }
+
+        std::stringstream vertex_src;
+        vertex_src << vertex_stream.rdbuf();
+        std::stringstream fragment_src;
+        fragment_src << fragmen_stream.rdbuf();
+
+        std::string v_src{ vertex_src.str() };
+        std::string f_src{ fragment_src.str() };
+
+        std::string_view v{ v_src };
+        std::string_view f{ f_src };
+
+        create(v, f);
+    }
+    catch (const std::exception& e)
+    {
+        std::clog << e.what() << std::endl;
+
+        throw std::runtime_error(
+            "can't create shader from: " + vertex_shader_path.u8string() + " " +
+            fragment_shader_path.u8string() + " cause: " + e.what());
+    }
+}
+
+shader::shader(shader&& other) noexcept
+    : program_id(other.program_id)
+{
+    other.program_id = 0;
+}
+
+shader& shader::operator=(shader&& other) noexcept
+{
+    shader tmp(std::move(other));
+
+    std::swap(tmp.program_id, program_id);
+    return *this;
+}
+shader::~shader() noexcept
+{
+    glDeleteProgram(program_id); // 0 will be silently ignored
+}
+
+void shader::use()
+{
+    assert(GL_TRUE == glIsProgram(program_id));
+    glUseProgram(program_id);
+}
+
+GLint get_uniform_index(std::string_view name, uint32_t program_id)
+{
+    assert(*(name.data() + name.size()) == '\0'); // null terminated string
+    GLint uniform_index = glGetUniformLocation(program_id, name.data());
+
+    if (uniform_index == -1)
+    {
+        throw std::runtime_error("can't find uniform: " + std::string(name));
+    }
+    return uniform_index;
+}
+
+void shader::set_uniform(std::string_view name, bool value)
+{
+    GLint uniform_index = get_uniform_index(name, program_id);
+    glUniform1i(uniform_index, value);
+}
+void shader::set_uniform(std::string_view name, std::int32_t value)
+{
+    GLint uniform_index = get_uniform_index(name, program_id);
+    glUniform1i(uniform_index, value);
+}
+void shader::set_uniform(std::string_view name, float value)
+{
+    GLint uniform_index = get_uniform_index(name, program_id);
+    glUniform1f(uniform_index, value);
+}
+void shader::set_uniform(std::string_view name, texture& tex,
+                         std::uint32_t index)
+{
+    GLint uniform_index = get_uniform_index(name, program_id);
+    glActiveTexture(GL_TEXTURE0 + index);
+
+    tex.bind();
+    glUniform1i(uniform_index, static_cast<int32_t>(index));
+}
+void shader::set_uniform(std::string_view name, const glm::mat4& m)
+{
+    GLint uniform_index = get_uniform_index(name, program_id);
+    glUniformMatrix4fv(uniform_index, 1, GL_FALSE, glm::value_ptr(m));
+}
+void shader::set_uniform(std::string_view name, const glm::mat3& m)
+{
+    GLint uniform_index = get_uniform_index(name, program_id);
+    glUniformMatrix3fv(uniform_index, 1, GL_FALSE, glm::value_ptr(m));
+}
+
+void shader::set_uniform(std::string_view name, const glm::vec3& v)
+{
+    GLint uniform_index = get_uniform_index(name, program_id);
+    glUniform3fv(uniform_index, 1, glm::value_ptr(v));
+}
+
+std::string shader::validate() noexcept(false)
+{
+    // validate current OpenGL state
+    glValidateProgram(program_id);
+
+    int success;
+    glGetProgramiv(program_id, GL_VALIDATE_STATUS, &success);
+
+    if (1 == success)
+    {
+        char info_log[4096] = { 0 };
+        glGetProgramInfoLog(program_id, sizeof(info_log), nullptr, info_log);
+
+        if (strlen(info_log) > 0)
+        {
+            return { info_log };
+        }
+    }
+    else
+    {
+        throw std::runtime_error("can't get validation status");
+    }
+    return {};
+}
+} // namespace gles30
