cb86bd90730ba028ceb920e631edc664b280cebf
Leanid Chaika <leanid.chayka@gmail.com>
2020-07-04 19:29:02 +0300

    better multithreading example in real life

--- class-00-basic-prog/07-multi-theading/main.cxx
+++ class-00-basic-prog/07-multi-theading/main.cxx
@@ -1,22 +1,68 @@
+#include <algorithm>
+#include <array>
 #include <cstdlib>
 #include <iostream>
 #include <thread>
+#include <fstream>
+#include <cstring>
+#include <numeric>
+#if __has_include(< syncstream >)
+#include <syncstream>
+#endif
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
+int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
 {
-    auto print_two_chars = [](char c0, char c1, size_t n) {
-        for (size_t i = 0; i < n; ++i)
+    using namespace std;
+    const char *tmp_file = "tmp_file.txt";
+    const string_view print_patterns[] = { "012\n", "abcd\n", "|||||\n" };
+    const size_t num_of_iterations = 1024;
+
+    ofstream out_file;
+    out_file.exceptions(ios::badbit | ios::failbit);
+    out_file.open(tmp_file, ios::binary | ios::trunc);
+
+    auto print_chars_unsafe = [&out_file](const string_view &str, size_t num_of_iter)
         {
-            std::cout << c0;
-            std::cout << c1;
-        }
-    };
+            for (size_t i = 0; i < num_of_iter; ++i)
+            {
+                out_file << str;
+                // fix: c++20
+                //std::osyncstream(out_file) << str;
+            }
+        };
 
-    std::thread first(print_two_chars, 'a', 'b', 1024);
-    std::thread second(print_two_chars, 'c', 'd', 1024);
+    array<thread, std::size(print_patterns)> threads;
 
-    first.join();
-    second.join();
+    std::transform(begin(print_patterns), end(print_patterns), begin(threads),
+            [&print_chars_unsafe, &num_of_iterations](const string_view &pattern)
+            {
+                return thread{ print_chars_unsafe, pattern, num_of_iterations };
+            });
 
+    for_each(begin(threads), end(threads), [](thread &t){ t.join(); });
+
+    out_file.close();
+
+    const size_t expected_size_of_file =
+        std::accumulate(begin(print_patterns), end(print_patterns), size_t{ 0 },
+                [](size_t value, const string_view &right)
+                {
+                    const size_t num_of_chars_for_pattern = right.size() * num_of_iterations;
+                    return value + num_of_chars_for_pattern;
+                });
+
+    ifstream in_file;
+    in_file.open(tmp_file, ios::binary | ios::ate);
+    const size_t file_size = in_file.tellg();
+
+    if (file_size == expected_size_of_file)
+    {
+        cout << "file_size(" << file_size << ") == expected_size_of_file(" << expected_size_of_file << ")";
+    }
+    else
+    {
+        cout << "file_size(" << file_size << ") != expected_size_of_file(" << expected_size_of_file << ")";
+    }
+    cout << endl;
     return 0;
 }
