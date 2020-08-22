e5b9f2e190acda3321fd6e1845da44874b00fba4
Leanid Chaika <leanid.chaika@gmail.com>
2020-08-22 23:34:56 +0300

    prepare template for next tutorial

---
+++ class-02-graphics-3d/23-03-uniform-buffers/gles30_shader.hxx
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <filesystem>
+#include <string>
+#include <string_view>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+namespace gles30
+{
+
+class texture;
+
+class shader
+{
+public:
+    shader(const std::filesystem::path& vertex_shader_path,
+           const std::filesystem::path& fragment_shader_path) noexcept(false);
+
+    void use();
+
+    void set_uniform(std::string_view name, bool value);
+    void set_uniform(std::string_view name, std::int32_t value);
+    void set_uniform(std::string_view name, float value);
+    void set_uniform(std::string_view name, texture& tex, std::uint32_t index);
+    void set_uniform(std::string_view name, const glm::mat4&);
+    void set_uniform(std::string_view name, const glm::mat3&);
+    void set_uniform(std::string_view name, const glm::vec3&);
+    void set_uniform(std::string_view name, const glm::vec2&);
+
+    /// just for debug purposes you can validate current state before
+    /// render geometry
+    std::string validate() noexcept(false);
+
+    shader(shader&&) noexcept;
+    shader& operator=(shader&&) noexcept;
+    ~shader() noexcept;
+
+    shader(const shader&) = delete;
+    shader& operator=(const shader&) = delete;
+
+private:
+    void          create(std::string_view vertex_shader_src,
+                         std::string_view fragment_shader_src) noexcept(false);
+    std::uint32_t program_id;
+};
+} // end namespace gles30
