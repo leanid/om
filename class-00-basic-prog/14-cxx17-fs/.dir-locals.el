09076916d7a6f7cb0fa98eede06fa89a84121d16
Leanid Chaika <leanid.chaika@gmail.com>
2021-10-04 03:26:34 +0300

    minimal example for emacs lsp-mode c++ development with cmake-ide too

---
+++ class-00-basic-prog/14-cxx17-fs/.dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((cmake-ide-project-dir . "~/fun/om/class-00-basic-prog/14-cxx17-fs/")
+         (cmake-ide-build-dir . "~/fun/om/class-00-basic-prog/14-cxx17-fs/build")
+         (cmake-ide-cmake-args . ("-DCMAKE_BUILD_TYPE=Debug"
+                                  "-DCMAKE_CXX_COMPILER=clang++"))
+;         (cmake-ide-cmake-opts . "-DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++")
+         )))
