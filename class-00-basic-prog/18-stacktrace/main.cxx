43ded731ab5ce76b6ce4a612ca8ef2e62655cb76
Leanid Chaika <leanid.chaika@gmail.com>
2022-05-14 00:11:43 +0300

    add c++23 <backtrace> example in cmake

---
+++ class-00-basic-prog/18-stacktrace/main.cxx
@@ -0,0 +1,24 @@
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <stacktrace> // --std=c++23 needed
+
+void call_other_func()
+{
+    std::stacktrace trace = std::stacktrace::current();
+    std::cerr << trace;
+}
+
+void some_func()
+{
+    call_other_func();
+}
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
+{
+    using namespace std;
+
+    some_func();
+
+    return cout.fail();
+}
---
+++ class-00-basic-prog/18-stacktrace/readme.md
@@ -0,0 +1,5 @@
+# Some info about *backtrace*
+1. tested only on Linux GNU g++ compiler 12
+2. you have to build with *-DCMAKE_BUILD_TYPE=Debug* or *-DCMAKE_BUILD_TYPE=RelWithDebInfo*
+3. if you build in release you see only last function name. 
+
