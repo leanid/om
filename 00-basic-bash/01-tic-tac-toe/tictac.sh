1dcab18ae8f1f10832df84e106b37158bd5b3fa1
Leanid Chaika <leanid.chaika@gmail.com>
2022-06-19 03:24:35 +0300

    continue making XO-game in bash

--- 00-basic-bash/01-tic-tac-toe/tictac.sh
+++ 00-basic-bash/01-tic-tac-toe/tictac.sh
@@ -14,6 +14,31 @@ function print_field()
     done
 }
 
+function check_horizontal()
+{
+    i0=$1 # $1 - start index of horizontal line 0, 3, 6
+    i1=$((i0+1))
+    i2=$((i0+2))
+
+    v0=${field[((i0))]}
+    v1=${field[((i1))]}
+    v2=${field[((i2))]}
+
+    if [ v0 == v1 ] && [ v1 == v2 ]
+    then
+        if [ v0 == "X" ]
+        then
+            echo "X - won!!!"
+            return 0;
+        elif [ v0 == "O" ]
+        then
+            echo "O - won!!!"
+            return 0;
+        fi
+    fi
+    return 1;
+}
+
 print_field
 
 echo "Where to put X? (1-9)"
