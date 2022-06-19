e2608da29fc848386a77d66a009a3b75e5b55e7f
Leanid Chaika <leanid.chaika@gmail.com>
2022-06-19 12:15:45 +0300

    XO-game works in bash script

--- 00-basic-bash/01-tic-tac-toe/tictac.sh
+++ 00-basic-bash/01-tic-tac-toe/tictac.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo play XY-game
+echo "play XO-game"
 
 field=(_ _ _ \
        _ _ _ \
@@ -14,37 +14,97 @@ function print_field()
     done
 }
 
-function check_horizontal()
+function check_3_indexes()
 {
-    i0=$1 # $1 - start index of horizontal line 0, 3, 6
-    i1=$((i0+1))
-    i2=$((i0+2))
+    i0=$1
+    i1=$2
+    i2=$3
 
     v0=${field[((i0))]}
     v1=${field[((i1))]}
     v2=${field[((i2))]}
 
-    if [ v0 == v1 ] && [ v1 == v2 ]
+    if [ $v0 == $v1 ] && [ $v1 == $v2 ]
     then
-        if [ v0 == "X" ]
+        if [ $v0 == "X" ]
         then
             echo "X - won!!!"
             return 0;
-        elif [ v0 == "O" ]
+        elif [ $v0 == "O" ]
         then
             echo "O - won!!!"
             return 0;
         fi
     fi
     return 1;
+
+}
+
+function check_horizontal()
+{
+    i0=$1 # $1 - start index of horizontal line 0, 3, 6
+    i1=$((i0+1))
+    i2=$((i0+2))
+
+    if check_3_indexes $i0 $i1 $i2
+    then
+        return 0
+    fi
+    return 1
+}
+
+function check_vertical()
+{
+    i0=$1 # $1 - start index of vertical line 0, 1, 2
+    i1=$((i0+3))
+    i2=$((i0+6))
+
+    if check_3_indexes $i0 $i1 $i2
+    then
+        return 0
+    fi
+    return 1
+}
+
+function check_all_horizontal()
+{
+    if check_horizontal 0 || check_horizontal 3 || check_horizontal 6
+    then
+        return 0;
+    fi
+    return 1;
+}
+
+function check_all_vertical()
+{
+    if check_vertical 0 || check_vertical 1 || check_vertical 2
+    then
+        return 0;
+    fi
+    return 1;
+}
+
+function check_win_state()
+{
+    if check_all_horizontal || check_all_vertical || check_3_indexes 0 4 8 || check_3_indexes 2 4 6
+    then
+        exit 0;
+    fi
+    return 1;
 }
 
 print_field
 
-echo "Where to put X? (1-9)"
-
-read position
-
-field[((position-1))]="X"
-
-print_field
+while [ true ]
+do
+    echo "Where to put X? (1-9)"
+    read position
+    field[((position-1))]="X"
+    print_field
+    check_win_state
+    echo "Where to put O? (1-9)"
+    read position
+    field[((position-1))]="O"
+    print_field
+    check_win_state
+done
