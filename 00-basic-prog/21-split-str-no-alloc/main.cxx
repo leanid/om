cf1ebe84a5399b042e18b21ec3cf7de22fc4af41
Leanid Chaika <l_chayka@wargaming.net>
2022-11-07 16:21:12 +0300

    no dynamic memory allocation split line of test into words delim - space

---
+++ 00-basic-prog/21-split-str-no-alloc/main.cxx
@@ -0,0 +1,90 @@
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <string_view>
+#include <utility>
+
+struct split_words
+{
+    std::string_view      text;
+    static constexpr char delim = ' ';
+
+    struct iterator
+    {
+        const std::string_view& text;
+        std::size_t             cur_pos;
+        std::size_t             end_pos;
+
+        std::string_view operator*() const
+        {
+            return { &text[cur_pos], end_pos - cur_pos };
+        }
+        bool operator==(const iterator& other) const
+        {
+            return cur_pos == other.cur_pos && end_pos == other.end_pos;
+        }
+        bool operator!=(const iterator& other) const
+        {
+            return !(*this == other);
+        }
+        iterator& operator++()
+        {
+            cur_pos = text.find_first_not_of(delim, end_pos);
+
+            if (cur_pos == std::string_view::npos)
+            {
+                cur_pos = text.size();
+                end_pos = cur_pos;
+                return *this;
+            }
+
+            end_pos = text.find(delim, cur_pos);
+
+            if (cur_pos == std::string_view::npos)
+            {
+                end_pos = text.size();
+            }
+
+            return *this;
+        }
+    };
+
+    [[nodiscard]] iterator begin() const
+    {
+        auto start = text.find_first_not_of(delim);
+        if (start == std::string_view::npos)
+        {
+            return iterator{ text, text.size(), text.size() };
+        }
+        auto end_word = text.find(delim, start);
+        if (end_word == std::string_view::npos)
+        {
+            end_word = text.size();
+        }
+        return iterator{ text, start, end_word };
+    }
+    [[nodiscard]] iterator end() const
+    {
+        return iterator{ text, text.size(), text.size() };
+    }
+};
+
+int main(int argc, char** argv)
+{
+    using namespace std::literals;
+    auto str = " there should be no memory allocation during parsing"
+               "  into words this line and you   should'n create any"
+               "  contaner                  for intermediate words  "sv;
+
+    auto comma = "";
+    for (std::string_view word : split_words{ str })
+    {
+        std::cout << std::exchange(comma, ",") << std::quoted(word);
+    }
+
+    auto only_spaces = "                   "sv;
+    for (std::string_view word : split_words{ only_spaces })
+    {
+        std::cout << "you will not see this line in output" << std::endl;
+    }
+}
