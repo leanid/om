e28803284acc22a28fc1f034b33889663d7f371c
Leanid Chaika <leanid.chaika@gmail.com>
2023-01-30 01:51:14 +0300

    better example of OS behavior with memory allocation

--- 00-basic-prog/22-no-alloc-memory/main.cxx
+++ 00-basic-prog/22-no-alloc-memory/main.cxx
@@ -1,8 +1,10 @@
+#include <algorithm>
 #include <cstddef>
 #include <iomanip>
 #include <iostream>
 #include <iterator>
 #include <numeric>
+#include <random>
 #include <string_view>
 #include <utility>
 
@@ -25,13 +27,30 @@ int main(int argc, char** argv)
     std::string command;
     while (std::cin >> command)
     {
+        if ("fill"sv == command)
+        {
+            std::cout << "start touching memory and write to it." << std::endl;
+            std::iota(&init_time_memory[0],
+                      &init_time_memory[sizeof(init_time_memory)],
+                      0);
+
+            std::cout << "random shuffle memory bytes." << std::endl;
+            std::random_device rd;
+            std::mt19937       g(rd());
+            std::shuffle(&init_time_memory[0],
+                         &init_time_memory[sizeof(init_time_memory)],
+                         g);
+            std::cout << "done" << std::endl;
+        }
         if ("init_mem"sv == command)
         {
+            std::cout << "sizeof(init_time_memory) == "sv
+                      << sizeof(init_time_memory) << std::endl;
             std::cout << std::accumulate(
                 &init_time_memory[0],
                 &init_time_memory[sizeof(init_time_memory)],
                 0);
-            std::cout << std::endl;
+            std::cout << "done" << std::endl;
         }
         if ("q"sv == command)
         {
