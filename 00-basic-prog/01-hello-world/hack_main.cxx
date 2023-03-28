0d9336e7ba719118210fef955d9bfaeef6dc4b87
Leanid Chaika <leanid.chaika@gmail.com>
2023-03-28 11:40:00 +0300

    minor fix hello world

--- 00-basic-prog/01-hello-world/hack_main.cxx
+++ 00-basic-prog/01-hello-world/hack_main.cxx
@@ -1,14 +1,14 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 
-int print(const char* message, int size)
+long int print(const char* message, int size)
 {
-    int exit_code = 0;
-    syscall(SYS_write, 1, message, size);
+    constexpr int stdout_stream = 1;
+    long int      exit_code = syscall(SYS_write, stdout_stream, message, size);
     return exit_code;
 }
 
-int main(int /*argc*/, char* /*argv*/ [])
+int main(int /*argc*/, char* /*argv*/[])
 {
     return print("hello world\n", 12);
 }
