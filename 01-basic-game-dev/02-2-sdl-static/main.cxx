b1636dc5ba403ddf93af82909aa44e83dd9ca439
Leanid Chaika <leanid.chaika@gmail.com>
2023-04-06 11:44:33 +0300

    update to SDL3 version 02 tutorial

--- 01-basic-game-dev/02-2-sdl-static/main.cxx
+++ 01-basic-game-dev/02-2-sdl-static/main.cxx
@@ -1,11 +1,7 @@
 #include <cstdlib>
 #include <iostream>
 
-#if __has_include(<SDL_version.h>)
-#include <SDL_version.h>
-#else
-#include <SDL2/SDL_version.h>
-#endif
+#include <SDL3/SDL_version.h>
 
 std::ostream& operator<<(std::ostream& out, const SDL_version& v)
 {
