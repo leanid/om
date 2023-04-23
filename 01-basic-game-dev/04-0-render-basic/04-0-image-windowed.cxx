34822ae4b315e1879650835ddbcae45ed386d618
leanid <leanid.chaika@ya.ru>
2023-04-24 01:05:46 +0300

    remove redundant includes

--- 01-basic-game-dev/04-0-render-basic/04-0-image-windowed.cxx
+++ 01-basic-game-dev/04-0-render-basic/04-0-image-windowed.cxx
@@ -2,8 +2,6 @@
 
 #include <SDL3/SDL.h>
 
-#include <SDL3/SDL_pixels.h>
-#include <SDL3/SDL_render.h>
 #include <bitset>
 #include <cmath>
 #include <cstdlib>
--- 01-basic-game-dev/04-0-render-basic/05-0-render-basic-windowed.cxx
+++ 01-basic-game-dev/04-0-render-basic/05-0-render-basic-windowed.cxx
@@ -2,7 +2,6 @@
 
 #include <SDL3/SDL.h>
 
-#include <SDL3/SDL_pixels.h>
 #include <cstdlib>
 #include <iostream>
 
