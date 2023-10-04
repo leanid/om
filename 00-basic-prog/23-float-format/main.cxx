1663ca34f4ebd15f1669550ca570f9a21896c108
leanid <leanid.chaika@ya.ru>
2023-10-04 23:14:10 +0300

    add float bits example

--- 00-basic-prog/23-float-format/main.cxx
+++ 00-basic-prog/23-float-format/main.cxx
@@ -1,6 +1,55 @@
+#include <bitset>
+#include <cmath>
+#include <cstdint>
+#include <iomanip>
 #include <iostream>
+#include <string>
+
+struct float_bits
+{
+    float value;
+};
+
+std::ostream& operator<<(std::ostream& out, const float_bits& value)
+{
+    using namespace std;
+    out << "sEEEEEEEEfffffffffffffffffffffff" << endl;
+    uint32_t as_i32   = *reinterpret_cast<const uint32_t*>(&value.value);
+    uint32_t sign_bit = as_i32 & 0x80'00'00'00; // 1 hi bit
+    uint32_t exp      = as_i32 & 0x7F'80'00'00; // 8 hi bit after "sign"
+    uint32_t fraction = as_i32 & 0x00'7F'FF'FF; // 23 hi bits after exp
+    sign_bit >>= 31;
+    bitset<1> sign_bits(sign_bit);
+    out << sign_bits << string(31, '_') << " sign 1 bit 0->(+) 1->(-)" << endl;
+    exp >>= 23;
+    bitset<8> exp_bits(exp);
+    int       real_exp    = exp - 127;
+    float     two_pow_exp = pow(2.0f, real_exp);
+    out << '_' << exp_bits << string(23, '_') << " exp=(x-127) " << exp
+        << "-127=" << real_exp << " 2^" << real_exp << "=" << two_pow_exp
+        << endl;
+    bitset<23> fraction_bits(fraction);
+    float      fraction_value = .0f;
+    for (int32_t i = 1; i <= 23; ++i)
+    {
+        if (fraction_bits[i])
+        {
+            float next_fraction =
+                pow(2.0f, -i); // 1/2 1/4 1/8 1/16 ... 1/(2^23)
+            fraction_value += next_fraction;
+        }
+    }
+    out << string(9, '_') << fraction_bits << " fraction 23 bits " << fixed
+        << setw(12) << fraction_value << endl;
+    const char* sign_char = sign_bit ? "-" : "";
+    out << "s*exp*(1+fraction)=" << sign_char << two_pow_exp << "*(1.0+"
+        << fraction_value << ")=" << sign_char
+        << two_pow_exp * (1.0f + fraction_value) << endl;
+    return out;
+}
 
 int main(int argc, char** argv)
 {
+    std::cout << float_bits{ 4.f };
     return std::cout.fail();
 }
