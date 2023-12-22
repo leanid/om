ddd0ba5d60291df476c36876b3d79a804dd17806
leanid <leanid.chaika@ya.ru>
2023-12-23 01:06:32 +0300

    Refactored and enhanced vk_render class in main.cxx
    This update overhauls the vk_render class with major internal restructuring, replacing 'std::exit' usage with 'std::runtime_error' and transferring main function's Vulkan-related initializations to the class's constructor. Also, it improved errors handling by properly validating the existence of expected extensions. Additionally, CMakeLists.txt is modified to require the C++20 standard for the project.

--- 02-vulkan/02-vk-instance/main.cxx
+++ 02-vulkan/02-vk-instance/main.cxx
@@ -1,24 +1,136 @@
+#include <algorithm>
 #include <cstdint>
 #include <cstdlib>
 #include <iomanip>
 #include <iostream>
-
-#include <source_location>
-#include <stdlib.h>
+#include <ostream>
+#include <stdexcept>
 #include <string_view>
-#include <vulkan/vulkan.hpp>
-#include <vulkan/vulkan_handles.hpp>
-#include <vulkan/vulkan_structs.hpp>
+
+#include <experimental/scope>
 
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_vulkan.h>
+
+#include <vulkan/vulkan.hpp>
+#include <vulkan/vulkan_handles.hpp>
+#include <vulkan/vulkan_structs.hpp>
+#include <vulkan/vulkan_to_string.hpp>
 
 namespace om
 {
 class vk_render
 {
 public:
+    using callback = const char* const* (*)(uint32_t* num_extensions);
+    struct hints
+    {
+        bool verbose;
+    };
+
+    explicit vk_render(std::ostream& log,
+                       callback      get_instance_extensions,
+                       hints         h)
+        : log{ log }
+    {
+        vk::ApplicationInfo    application_info;
+        vk::InstanceCreateInfo instance_create_info;
+        instance_create_info.pApplicationInfo        = &application_info;
+        instance_create_info.ppEnabledExtensionNames = get_instance_extensions(
+            &instance_create_info.enabledExtensionCount);
+
+        if (nullptr == instance_create_info.ppEnabledExtensionNames)
+        {
+            throw std::runtime_error(
+                "get_instance_extensions callback return nullptr");
+        }
+
+        if (h.verbose)
+        {
+            log << "minimal vulkan expected extensions from "
+                   "get_instance_extensions callback\n";
+            std::for_each_n(instance_create_info.ppEnabledExtensionNames,
+                            instance_create_info.enabledExtensionCount,
+                            [&log](std::string_view instance_extension)
+                            { log << instance_extension << '\n'; });
+        }
+
+        validate_expected_extensions_exists(instance_create_info, h);
+
+        // TODO future all layers (maybe debug?)
+        // instance_create_info.enabledLayerCount   = 0;
+        // instance_create_info.ppEnabledLayerNames = nullptr;
+
+        instance = vk::createInstance(instance_create_info);
+    }
+
 private:
+    void validate_expected_extensions_exists(
+        const vk::InstanceCreateInfo& instance_create_info, const hints& h)
+    {
+        uint32_t   num_extensions{};
+        vk::Result result = vk::enumerateInstanceExtensionProperties(
+            nullptr, &num_extensions, nullptr);
+
+        if (result != vk::Result::eSuccess)
+        {
+            throw std::runtime_error(
+                "error: enumerateInstanceExtensionProperties returned: " +
+                vk::to_string(result));
+        }
+
+        if (h.verbose)
+        {
+            log << " num of vulkan extension on this machine: ["
+                << num_extensions << "]" << std::endl;
+        }
+
+        std::vector<vk::ExtensionProperties> extension_properties(
+            num_extensions);
+
+        result = vk::enumerateInstanceExtensionProperties(
+            nullptr, &num_extensions, extension_properties.data());
+
+        if (result != vk::Result::eSuccess)
+        {
+            throw std::runtime_error(
+                "error: second enumerateInstanceExtensionProperties "
+                "returned: " +
+                vk::to_string(result));
+        }
+
+        if (h.verbose)
+        {
+            log << "all instance extensions: \n";
+            std::for_each(extension_properties.begin(),
+                          extension_properties.end(),
+                          [this](const vk::ExtensionProperties& extension)
+                          {
+                              log << std::setw(3) << extension.specVersion
+                                  << ' ' << extension.extensionName << '\n';
+                          });
+        }
+        std::for_each_n(
+            instance_create_info.ppEnabledExtensionNames,
+            instance_create_info.enabledExtensionCount,
+            [&extension_properties](std::string_view extension)
+            {
+                auto it = std::find_if(
+                    extension_properties.begin(),
+                    extension_properties.end(),
+                    [extension](const vk::ExtensionProperties& other_extension)
+                    { return other_extension.extensionName == extension; });
+                if (it == extension_properties.end())
+                {
+                    throw std::runtime_error(
+                        "error: can't find Instance Extension: " +
+                        std::string(extension));
+                }
+            });
+    }
+
+    std::ostream& log;
+    vk::Instance  instance;
 };
 } // namespace om
 
@@ -29,76 +141,18 @@ int main(int argc, char** argv)
         std::cerr << SDL_GetError();
         return EXIT_FAILURE;
     }
+    std::experimental::scope_exit quit([]() { SDL_Quit(); });
 
     if (0 != SDL_Vulkan_LoadLibrary(nullptr))
     {
         std::cerr << SDL_GetError();
         return EXIT_FAILURE;
     }
+    std::experimental::scope_exit unload([]() { SDL_Vulkan_UnloadLibrary(); });
 
-    vk::ApplicationInfo    application_info;
-    vk::InstanceCreateInfo instance_create_info;
-    instance_create_info.pApplicationInfo = &application_info;
-    instance_create_info.ppEnabledExtensionNames =
-        SDL_Vulkan_GetInstanceExtensions(
-            &instance_create_info.enabledExtensionCount);
+    om::vk_render render(std::cerr,
+                         SDL_Vulkan_GetInstanceExtensions,
+                         om::vk_render::hints{ .verbose = true });
 
-    std::cout << "minimal extensions from SDL helper\n";
-    for (unsigned i = 0; i < instance_create_info.enabledExtensionCount; ++i)
-    {
-        std::cout << instance_create_info.ppEnabledExtensionNames[i] << '\n';
-    }
-
-    uint32_t   num_extensions{};
-    vk::Result result = vk::enumerateInstanceExtensionProperties(
-        nullptr, &num_extensions, nullptr);
-
-    std::cout << "result=" << result
-              << " num of vulkan extension on my machine: [" << num_extensions
-              << "]" << std::endl;
-
-    std::vector<vk::ExtensionProperties> extension_properties(num_extensions);
-
-    result = vk::enumerateInstanceExtensionProperties(
-        nullptr, &num_extensions, extension_properties.data());
-
-    assert(result == vk::Result::eSuccess);
-
-    std::for_each(extension_properties.begin(),
-                  extension_properties.end(),
-                  [](const vk::ExtensionProperties& extension)
-                  {
-                      std::cout << std::setw(3) << extension.specVersion << ' '
-                                << extension.extensionName << '\n';
-                  });
-
-    // TODO future all layers (maybe debug?)
-    instance_create_info.enabledLayerCount   = 0;
-    instance_create_info.ppEnabledLayerNames = nullptr;
-
-    vk::Instance instance = vk::createInstance(instance_create_info);
-
-    // validate that extensions from SDL_Vulkan_GetInstanceExtensions
-    // present in all extensions
-
-    std::for_each(
-        instance_create_info.ppEnabledExtensionNames,
-        instance_create_info.ppEnabledExtensionNames +
-            instance_create_info.enabledExtensionCount,
-        [&extension_properties](std::string_view extension)
-        {
-            auto it = std::find_if(
-                extension_properties.begin(),
-                extension_properties.end(),
-                [extension](const vk::ExtensionProperties& other_extension)
-                { return other_extension.extensionName == extension; });
-            if (it == extension_properties.end())
-            {
-                std::cerr << "error: can't find Instance Extension: "
-                          << extension << '\n';
-                std::exit(EXIT_FAILURE);
-            }
-        });
-
-    return std::cout.fail();
+    return std::cerr.fail();
 }
