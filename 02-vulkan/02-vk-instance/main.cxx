c267a501b0561be3cfb84aa101259bfb954e658a
leanid <leanid.chaika@ya.ru>
2023-12-22 22:00:28 +0300

    compare vk extensions from SDL and all

--- 02-vulkan/02-vk-instance/main.cxx
+++ 02-vulkan/02-vk-instance/main.cxx
@@ -1,9 +1,11 @@
-#include "SDL_error.h"
-#include "SDL_init.h"
 #include <cstdint>
+#include <cstdlib>
+#include <iomanip>
 #include <iostream>
 
+#include <source_location>
 #include <stdlib.h>
+#include <string_view>
 #include <vulkan/vulkan.hpp>
 #include <vulkan/vulkan_handles.hpp>
 #include <vulkan/vulkan_structs.hpp>
@@ -47,17 +49,56 @@ int main(int argc, char** argv)
         std::cout << instance_create_info.ppEnabledExtensionNames[i] << '\n';
     }
 
-    uint32_t num_extensions{};
-    std::ignore = vk::enumerateInstanceExtensionProperties(
+    uint32_t   num_extensions{};
+    vk::Result result = vk::enumerateInstanceExtensionProperties(
         nullptr, &num_extensions, nullptr);
-    std::cout << "num of vulkan extension on my machine: [" << num_extensions
+
+    std::cout << "result=" << result
+              << " num of vulkan extension on my machine: [" << num_extensions
               << "]" << std::endl;
+
+    std::vector<vk::ExtensionProperties> extension_properties(num_extensions);
+
+    result = vk::enumerateInstanceExtensionProperties(
+        nullptr, &num_extensions, extension_properties.data());
+
+    assert(result == vk::Result::eSuccess);
+
+    std::for_each(extension_properties.begin(),
+                  extension_properties.end(),
+                  [](const vk::ExtensionProperties& extension)
+                  {
+                      std::cout << std::setw(3) << extension.specVersion << ' '
+                                << extension.extensionName << '\n';
+                  });
 
     // TODO future all layers (maybe debug?)
     instance_create_info.enabledLayerCount   = 0;
     instance_create_info.ppEnabledLayerNames = nullptr;
 
     vk::Instance instance = vk::createInstance(instance_create_info);
-    // instance.
+
+    // validate that extensions from SDL_Vulkan_GetInstanceExtensions
+    // present in all extensions
+
+    std::for_each(
+        instance_create_info.ppEnabledExtensionNames,
+        instance_create_info.ppEnabledExtensionNames +
+            instance_create_info.enabledExtensionCount,
+        [&extension_properties](std::string_view extension)
+        {
+            auto it = std::find_if(
+                extension_properties.begin(),
+                extension_properties.end(),
+                [extension](const vk::ExtensionProperties& other_extension)
+                { return other_extension.extensionName == extension; });
+            if (it == extension_properties.end())
+            {
+                std::cerr << "error: can't find Instance Extension: "
+                          << extension << '\n';
+                std::exit(EXIT_FAILURE);
+            }
+        });
+
     return std::cout.fail();
 }
