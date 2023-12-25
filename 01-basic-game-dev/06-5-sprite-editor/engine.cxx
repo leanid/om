bcdd2c5b20001476a898b4d140013f141bf6c87c
leanid chaika <leanid.chaika@gmail.com>
2023-12-25 03:08:34 +0300

    fix compilation on Mac OS

--- 01-basic-game-dev/06-5-sprite-editor/engine.cxx
+++ 01-basic-game-dev/06-5-sprite-editor/engine.cxx
@@ -22,7 +22,7 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_opengl.h>
 #include <SDL3/SDL_opengl_glext.h>
-#include <SDL3/SDL_syswm.h>
+// #include <SDL3/SDL_syswm.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #ifdef __GNUG__
