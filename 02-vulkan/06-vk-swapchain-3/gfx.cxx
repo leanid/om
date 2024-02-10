c2605cd75c3092b6c61b3c4cc86d2021acc7edb2
leanid <leanid.chaika@ya.ru>
2024-02-11 00:34:12 +0300

    continue refactoring

--- 02-vulkan/06-vk-swapchain-3/gfx.cxx
+++ 02-vulkan/06-vk-swapchain-3/gfx.cxx
@@ -80,7 +80,7 @@ void gfx::create_instance(callback_get_ext get_instance_extensions)
 }
 
 void gfx::validate_expected_extensions_exists(
-    const vk::InstanceCreateInfo& instance_create_info)
+    const vk::InstanceCreateInfo& create_info)
 {
     uint32_t   num_extensions{};
     vk::Result result = vk::enumerateInstanceExtensionProperties(
@@ -119,8 +119,8 @@ void gfx::validate_expected_extensions_exists(
                   });
 
     std::for_each_n(
-        instance_create_info.ppEnabledExtensionNames,
-        instance_create_info.enabledExtensionCount,
+        create_info.ppEnabledExtensionNames,
+        create_info.enabledExtensionCount,
         [&extension_properties](std::string_view extension)
         {
             auto it = std::find_if(
@@ -138,4 +138,487 @@ void gfx::validate_expected_extensions_exists(
         });
 }
 
+void gfx::validate_instance_layer_present(std::string_view instance_layer)
+{
+    uint32_t   layer_count{};
+    vk::Result r = vk::enumerateInstanceLayerProperties(&layer_count, nullptr);
+    if (r != vk::Result::eSuccess)
+    {
+        throw std::runtime_error("error: can't get instance layers count");
+    }
+    std::vector<vk::LayerProperties> available_layers(layer_count);
+    r = vk::enumerateInstanceLayerProperties(&layer_count,
+                                             available_layers.data());
+    if (r != vk::Result::eSuccess)
+    {
+        throw std::runtime_error("error: can't get any instance layers");
+    }
+
+    log << "all vulkan layers count [" << layer_count << "]\n";
+    log << "spec-version | impl-version | name and description\n";
+    std::for_each(available_layers.begin(),
+                  available_layers.end(),
+                  [this](const vk::LayerProperties& layer)
+                  {
+                      log << api_version_to_string(layer.specVersion) << ' '
+                          << layer.implementationVersion << ' '
+                          << layer.layerName << " " << layer.description
+                          << '\n';
+                  });
+    auto it = std::find_if(available_layers.begin(),
+                           available_layers.end(),
+                           [&instance_layer](const vk::LayerProperties& layer)
+                           { return layer.layerName == instance_layer; });
+
+    if (it == available_layers.end())
+    {
+        log << "see: support/vulkan/install.md how to install validation "
+               "layer\n";
+        throw std::runtime_error("error: can't find requested layer: " +
+                                 std::string(instance_layer));
+    }
+}
+
+static bool check_render_queue(const vk::QueueFamilyProperties& property)
+{
+    return property.queueFlags & vk::QueueFlagBits::eGraphics &&
+           property.queueCount >= 1;
+}
+
+static auto find_render_queue(
+    const std::vector<vk::QueueFamilyProperties>& queue_properties)
+{
+    return std::ranges::find_if(queue_properties, check_render_queue);
+}
+
+static auto find_render_queue(
+    const std::vector<vk::QueueFamilyProperties>& queue_properties,
+    const vk::PhysicalDevice&                     device,
+    const vk::SurfaceKHR&                         surface_to_check)
+{
+    return std::ranges::find_if(
+        queue_properties,
+        [&device, &surface_to_check, &queue_properties](
+            const vk::QueueFamilyProperties& property)
+        {
+            uint32_t index = std::distance(queue_properties.data(), &property);
+            return device.getSurfaceSupportKHR(index, surface_to_check);
+        });
+}
+
+bool gfx::check_device_suitable(vk::PhysicalDevice& physical)
+{
+    std::vector<vk::QueueFamilyProperties> queue_properties =
+        physical.getQueueFamilyProperties();
+
+    auto it = find_render_queue(queue_properties);
+
+    bool render_queue_found   = it != queue_properties.end();
+    bool all_extensions_found = std::ranges::all_of(
+        device_extensions,
+        [&physical](const char* extension_name)
+        { return check_device_extension_supported(physical, extension_name); });
+    return render_queue_found && all_extensions_found;
+}
+
+bool gfx::check_device_extension_supported(vk::PhysicalDevice& device,
+                                           std::string_view    extension_name)
+{
+    auto extensions = device.enumerateDeviceExtensionProperties();
+    auto it         = std::ranges::find_if(
+        extensions,
+        [&extension_name](const auto& extension)
+        { return extension.extensionName == extension_name; });
+    return it != extensions.end();
+}
+
+void gfx::get_physical_device()
+{
+    using namespace std::ranges;
+    std::vector<vk::PhysicalDevice> physical_devices =
+        instance.enumeratePhysicalDevices();
+
+    if (physical_devices.empty())
+    {
+        throw std::runtime_error(
+            "error: no any vulkan physical device found in the system");
+    }
+
+    log << "vulkan physical devises in the system:\n";
+    for_each(physical_devices,
+             [this](const vk::PhysicalDevice& device)
+             {
+                 const auto& properties = device.getProperties();
+
+                 auto api_version =
+                     api_version_to_string(properties.apiVersion);
+
+                 log << "id: " << properties.deviceID << '\n'
+                     << "device_name: " << properties.deviceName << '\n'
+                     << "device_type: " << vk::to_string(properties.deviceType)
+                     << '\n'
+                     << "api_version: " << api_version << '\n';
+             });
+    // find first suitable device
+    auto it = find_if(physical_devices,
+                      [this](vk::PhysicalDevice& physical)
+                      { return check_device_suitable(physical); });
+    if (it == physical_devices.end())
+    {
+        throw std::runtime_error(
+            "error: no physical devices found with render queue");
+    }
+
+    devices.physical = *it;
+
+    log << "selected device: " << devices.physical.getProperties().deviceName
+        << '\n';
+
+    queue_indexes.graphics_family =
+        get_render_queue_family_index(devices.physical);
+
+    vk::Bool32 is_supported = devices.physical.getSurfaceSupportKHR(
+        queue_indexes.graphics_family, surface);
+
+    if (!is_supported)
+    {
+        log << "error: physical device ["
+            << devices.physical.getProperties().deviceName
+            << "] with render queue index [" << queue_indexes.graphics_family
+            << "] do not support SurfaceSupportKHR\n"
+            << "try to find another queue index with VkSurfaceKHR\n";
+        queue_indexes.presentation_family =
+            get_presentation_queue_family_index(devices.physical, surface);
+    }
+    else
+    {
+        log << "render queue family with index ["
+            << queue_indexes.graphics_family << "] support surfaceKHR\n";
+
+        queue_indexes.presentation_family = queue_indexes.graphics_family;
+    }
+
+    auto queue_properties = devices.physical.getQueueFamilyProperties();
+
+    const vk::QueueFamilyProperties& render_queue_properties =
+        queue_properties.at(queue_indexes.graphics_family);
+
+    log << "render queue found with index is: " << queue_indexes.graphics_family
+        << '\n'
+        << "render queue count: " << render_queue_properties.queueCount << '\n'
+        << "presentation queue index is: " << queue_indexes.presentation_family
+        << '\n';
+}
+std::string gfx::api_version_to_string(uint32_t apiVersion)
+{
+    std::stringstream version;
+    version << VK_VERSION_MAJOR(apiVersion) << '.'
+            << VK_VERSION_MINOR(apiVersion) << '.'
+            << VK_VERSION_PATCH(apiVersion);
+    return version.str();
+}
+uint32_t gfx::get_render_queue_family_index(
+    const vk::PhysicalDevice& physical_device)
+{
+    auto queue_properties = physical_device.getQueueFamilyProperties();
+    auto it_render_queue  = find_render_queue(queue_properties);
+
+    if (it_render_queue == queue_properties.end())
+    {
+        using namespace std::literals;
+        throw std::runtime_error(
+            "error: can't find render queue for device: "s +
+            physical_device.getProperties().deviceName.data());
+    }
+
+    size_t graphics_queue_index =
+        std::distance(queue_properties.cbegin(), it_render_queue);
+    return static_cast<uint32_t>(graphics_queue_index);
+}
+uint32_t gfx::get_presentation_queue_family_index(
+    const vk::PhysicalDevice& physical_device,
+    const vk::SurfaceKHR&     surface_to_check)
+{
+    auto queue_properties = physical_device.getQueueFamilyProperties();
+    auto it_render_queue =
+        find_render_queue(queue_properties, physical_device, surface_to_check);
+
+    if (it_render_queue == queue_properties.end())
+    {
+        throw std::runtime_error("error: can't find presentation queue family");
+    }
+
+    size_t graphics_queue_index =
+        std::distance(queue_properties.cbegin(), it_render_queue);
+    return static_cast<uint32_t>(graphics_queue_index);
+}
+void gfx::create_surface(const callback_create_surface& create_vk_surface)
+{
+    VkSurfaceKHR surfaceKHR = create_vk_surface(instance, nullptr);
+    if (surfaceKHR == nullptr)
+    {
+        throw std::runtime_error(
+            "error: can't create VkSurfaceKHR from user provided callback");
+    }
+
+    log << "vk surface KHR created\n";
+    surface = surfaceKHR;
+}
+void gfx::validate_physical_device()
+{
+    // check properties
+    // devices.physical.getProperties();
+    // check features
+    // devices.physical.getFeatures().geometryShader
+    if (!queue_indexes.is_valid())
+    {
+        throw std::runtime_error("error: vulkan queue with render graphics "
+                                 "capability not found");
+    }
+
+    swapchain_details_t swap_chain_details =
+        get_swapchain_details(devices.physical);
+
+    log << swap_chain_details;
+
+    if (swap_chain_details.presentation_modes.empty())
+    {
+        throw std::runtime_error("error: presentation_modes empty");
+    }
+
+    if (swap_chain_details.surface_formats.empty())
+    {
+        throw std::runtime_error("error: surface_formats empty");
+    }
+}
+
+void gfx::create_logical_device()
+{
+    std::set<uint32_t> queue_family_indexes = {
+        queue_indexes.graphics_family, queue_indexes.presentation_family
+    };
+
+    std::vector<vk::DeviceQueueCreateInfo> queue_infos;
+
+    float priorities = 1.f;
+    for (uint32_t queue_index : queue_family_indexes)
+    {
+        vk::DeviceQueueCreateInfo device_queue_create_info;
+        device_queue_create_info.queueFamilyIndex = queue_index;
+        device_queue_create_info.queueCount       = 1;
+        // 1 - hi, 0 - lowest
+        device_queue_create_info.pQueuePriorities = &priorities;
+
+        queue_infos.push_back(device_queue_create_info);
+    }
+
+    vk::PhysicalDeviceFeatures device_features;
+
+    vk::DeviceCreateInfo device_create_info;
+    device_create_info.queueCreateInfoCount =
+        static_cast<uint32_t>(queue_infos.size());
+    device_create_info.pQueueCreateInfos = queue_infos.data();
+    device_create_info.enabledExtensionCount =
+        static_cast<uint32_t>(device_extensions.size());
+    device_create_info.ppEnabledExtensionNames = device_extensions.data();
+    device_create_info.enabledLayerCount = 0; // in vk_1_1 this in instance
+    device_create_info.pEnabledFeatures  = &device_features;
+
+    devices.logical = devices.physical.createDevice(device_create_info);
+    log << "logical device created\n";
+
+    uint32_t queue_index = 0;
+    render_queue =
+        devices.logical.getQueue(queue_indexes.graphics_family, queue_index);
+    log << "got render queue\n";
+    presentation_queue = devices.logical.getQueue(
+        queue_indexes.presentation_family, queue_index);
+    log << "got presentation queue\n";
+}
+void gfx::create_swapchain()
+{
+    swapchain_details_t swapchain_details =
+        get_swapchain_details(devices.physical);
+
+    // 1. choose best surface format
+    vk::SurfaceFormatKHR selected_format =
+        choose_best_surface_format(swapchain_details.surface_formats);
+
+    log << "best surface format we choose: "
+        << vk::to_string(selected_format.format) << ' '
+        << vk::to_string(selected_format.colorSpace) << std::endl;
+    // 2. choose best presentation mode
+    vk::PresentModeKHR selected_presentation_mode =
+        choose_best_present_mode(swapchain_details.presentation_modes);
+    log << "best presentation_mode we choose: "
+        << vk::to_string(selected_presentation_mode) << std::endl;
+    // 3. choose swapchain image resolution
+    vk::Extent2D selected_image_resolution =
+        choose_best_swapchain_image_resolution(
+            swapchain_details.surface_capabilities);
+    log << "current windows image resolution: "
+        << selected_image_resolution.width << 'x'
+        << selected_image_resolution.height << std::endl;
+
+    const auto& surface_capabilities = swapchain_details.surface_capabilities;
+
+    uint32_t image_count = surface_capabilities.minImageCount + 1u;
+    // if 0 - then limitless
+    if (surface_capabilities.maxImageCount > 0u &&
+        surface_capabilities.maxImageCount < image_count)
+    {
+        image_count = surface_capabilities.maxImageCount;
+    }
+
+    log << "image_count in swapchain minImageCount + 1 (triple buffering "
+           "expected) clamped by "
+           "maxImageCount = "
+        << image_count << std::endl;
+
+    vk::SwapchainCreateInfoKHR create_info;
+    create_info.imageFormat     = selected_format.format;
+    create_info.imageColorSpace = selected_format.colorSpace;
+    create_info.presentMode     = selected_presentation_mode;
+    create_info.imageExtent     = selected_image_resolution;
+    create_info.minImageCount   = image_count;
+    // number of layers for each image in chain
+    create_info.imageArrayLayers = 1u;
+    create_info.imageUsage       = vk::ImageUsageFlagBits::eColorAttachment;
+    create_info.preTransform     = surface_capabilities.currentTransform;
+    // how two windows will be composite together
+    create_info.compositeAlpha = vk::CompositeAlphaFlagBitsKHR::eOpaque;
+    create_info.clipped        = vk::True;
+
+    // if Graphics and Presentation families are different, then swapchain
+    // must let images be shared between families
+    if (queue_indexes.graphics_family != queue_indexes.presentation_family)
+    {
+        log << "graphics_family != presentation_family use sharing_mode: "
+            << vk::to_string(vk::SharingMode::eConcurrent) << std::endl;
+        create_info.imageSharingMode    = vk::SharingMode::eConcurrent;
+        std::array<uint32_t, 2> indexes = { queue_indexes.graphics_family,
+                                            queue_indexes.presentation_family };
+        create_info.pQueueFamilyIndices = indexes.data();
+        create_info.queueFamilyIndexCount =
+            static_cast<uint32_t>(indexes.size());
+    }
+    else
+    {
+        create_info.imageSharingMode      = vk::SharingMode::eExclusive;
+        create_info.pQueueFamilyIndices   = nullptr;
+        create_info.queueFamilyIndexCount = 0;
+    }
+
+    // if old swapchain been destroyed and this one replaces it, then link
+    // old one to quickly hand over responsibilities
+    create_info.oldSwapchain = nullptr;
+    create_info.surface      = surface;
+
+    swapchain = devices.logical.createSwapchainKHR(create_info);
+    log << "vulkan swapchain created\n";
+
+    // store for later usages
+    swapchain_image_format = create_info.imageFormat;
+    swapchain_image_extent = create_info.imageExtent;
+    log << "store in utilities swapchain_image_format: "
+        << vk::to_string(swapchain_image_format) << '\n'
+        << "swapchain_image_extent: " << swapchain_image_extent.width << 'x'
+        << swapchain_image_extent.height << std::endl;
+
+    swapchain_images = devices.logical.getSwapchainImagesKHR(swapchain);
+    log << "get swapchain images count: " << swapchain_images.size()
+        << std::endl;
+
+    swapchain_image_views.clear();
+    std::ranges::transform(
+        swapchain_images,
+        std::back_inserter(swapchain_image_views),
+        [this](vk::Image image) -> vk::ImageView
+        {
+            return create_image_view(
+                image, swapchain_image_format, vk::ImageAspectFlagBits::eColor);
+        });
+    log << "create swapchain_image_views count: "
+        << swapchain_image_views.size() << std::endl;
+}
+vk::Extent2D gfx::choose_best_swapchain_image_resolution(
+    const vk::SurfaceCapabilitiesKHR& capabilities)
+{
+    auto extent = capabilities.currentExtent;
+    if (extent.width != std::numeric_limits<uint32_t>::max() &&
+        extent.height != std::numeric_limits<uint32_t>::max())
+    {
+        log << "use extent2d from surface\n";
+        // no need to clamp
+        return extent;
+    }
+
+    uint32_t width{};
+    uint32_t height{};
+
+    get_window_buffer_size_(&width, &height);
+
+    extent.width  = width;
+    extent.height = height;
+
+    log << "use extent2d from callback_get_window_buffer_size: " << width << 'x'
+        << height << std::endl;
+
+    auto clamp_extent = [](vk::Extent2D&       extent,
+                           const vk::Extent2D& min_extent,
+                           const vk::Extent2D& max_extent)
+    {
+        extent.width =
+            std::clamp(extent.width, min_extent.width, max_extent.width);
+        extent.height =
+            std::clamp(extent.height, min_extent.height, max_extent.height);
+    };
+
+    clamp_extent(
+        extent, capabilities.minImageExtent, capabilities.maxImageExtent);
+    return extent;
+}
+
+vk::SurfaceFormatKHR gfx::choose_best_surface_format(
+    std::span<vk::SurfaceFormatKHR> formats)
+{
+    vk::SurfaceFormatKHR default_format(vk::Format::eR8G8B8A8Unorm,
+                                        vk::ColorSpaceKHR::eSrgbNonlinear);
+
+    if (formats.empty())
+    {
+        throw std::runtime_error("empty surface formats");
+    }
+
+    if (formats.size() == 1 && formats.front().format == vk::Format::eUndefined)
+    {
+        // this means all formats are supported!
+        // so let's use our defaults
+        return default_format;
+    }
+    // not all supported search for RGB or BGR
+    vk::SurfaceFormatKHR suitable_formats[] = {
+        default_format,
+        { vk::Format::eB8G8R8A8Unorm, vk::ColorSpaceKHR::eSrgbNonlinear }
+    };
+    auto it = std::ranges::find_first_of(formats, std::span(suitable_formats));
+
+    if (it != formats.end())
+    {
+        return *it;
+    }
+
+    // can't find suitable format lets try first as it is
+    return formats.front();
+}
+vk::PresentModeKHR gfx::choose_best_present_mode(
+    std::span<vk::PresentModeKHR> present_modes)
+{
+    if (std::ranges::contains(present_modes, vk::PresentModeKHR::eMailbox))
+    {
+        return vk::PresentModeKHR::eMailbox;
+    }
+    // guaranteed to be in any vulkan implementation
+    return vk::PresentModeKHR::eFifo;
+}
 } // namespace om
