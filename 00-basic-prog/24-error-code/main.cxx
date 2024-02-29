23318e5ba97cced5e6f74e00dfb897c589aa90e4
Leanid Chaika <leanid.chaika@gmail.com>
2024-03-01 00:48:07 +0300

    add more to example

--- 00-basic-prog/24-error-code/main.cxx
+++ 00-basic-prog/24-error-code/main.cxx
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <stdexcept>
 #include <system_error>
+#include <type_traits>
 
 namespace om
 {
-enum class custom_error_type
+enum class custom_errc
 {
     some_strange_error,
     other_error_we_got,
@@ -15,7 +16,7 @@ enum class custom_error_type
 
 namespace std
 {
-std::error_code make_error_code(om::custom_error_type err) noexcept
+std::error_code make_error_code(om::custom_errc err) noexcept
 {
     class custom_category : public error_category
     {
@@ -23,16 +24,16 @@ std::error_code make_error_code(om::custom_error_type err) noexcept
         const char* name() const noexcept override { return "custom"; }
         string      message(int err) const override
         {
-            auto code = static_cast<om::custom_error_type>(err);
+            auto code = static_cast<om::custom_errc>(err);
             switch (code)
             {
-                case om::custom_error_type::some_strange_error:
+                case om::custom_errc::some_strange_error:
                     return "some strange error";
-                case om::custom_error_type::other_error_we_got:
+                case om::custom_errc::other_error_we_got:
                     return "other error we got";
-                case om::custom_error_type::new_one_error:
+                case om::custom_errc::new_one_error:
                     return "new one error";
-                case om::custom_error_type::more_error_kind:
+                case om::custom_errc::more_error_kind:
                     return "more error kind";
             }
             throw std::invalid_argument("error: now such enum value");
@@ -43,6 +44,13 @@ std::error_code make_error_code(om::custom_error_type err) noexcept
 
     return std::error_code(static_cast<int>(err), category);
 }
+
+/// this code marks 'om::custom_errc' can be automatically converted to
+/// std::error_code or std::error_condition
+template <> struct is_error_code_enum<om::custom_errc> : public true_type
+{
+};
+
 } // namespace std
 
 int main(int argc, char** argv)
@@ -51,9 +59,13 @@ int main(int argc, char** argv)
                  "error_category:\n";
 
     std::error_code code =
-        std::make_error_code(om::custom_error_type::some_strange_error);
+        std::make_error_code(om::custom_errc::some_strange_error);
     std::cout << "custom error_code code = " << code
               << " message:" << code.message() << std::endl;
+    std::cout << " default error condition category name: "
+              << code.default_error_condition().category().name() << std::endl;
+    std::cout << " default error condition message: "
+              << code.default_error_condition().message() << std::endl;
 
     std::cout << "2. example posix errno codes\n";
 
