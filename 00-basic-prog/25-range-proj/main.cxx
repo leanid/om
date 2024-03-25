054085528d81d8da7c4291b486d01054b622c680
Leanid Chaika <leanid.chaika@gmail.com>
2024-03-25 15:52:37 +0300

    add range + projected example

---
+++ 00-basic-prog/25-range-proj/main.cxx
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace om
+{
+struct table_row
+{
+    std::uint32_t uid;
+    std::string   name;
+    std::string   info;
+};
+} // namespace om
+
+int main(int argc, char** argv)
+{
+    using namespace std;
+
+    vector<om::table_row> table = { { 1, "leo", "developer" },
+                                    { 2, "dima", "developer" },
+                                    { 3, "igor", "developer" } };
+
+    auto it = std::ranges::find(table, "igor", &om::table_row::name);
+
+    cout << "uid: " << it->uid << '\n'
+         << "name: " << it->name << '\n'
+         << "info: " << it->info << '\n';
+    return cout.fail();
+}
