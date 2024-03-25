c2fe884c60ebd764cc2a8f9386215c9e4dd21151
Leanid Chaika <leanid.chaika@gmail.com>
2024-03-25 21:25:22 +0300

    simpify range::find example

--- 00-basic-prog/25-range-proj/main.cxx
+++ 00-basic-prog/25-range-proj/main.cxx
@@ -1,30 +1,26 @@
 #include <algorithm>
+#include <cstdint>
 #include <iostream>
 #include <string>
 #include <vector>
 
-namespace om
-{
 struct table_row
 {
     std::uint32_t uid;
     std::string   name;
     std::string   info;
 };
-} // namespace om
 
 int main(int argc, char** argv)
 {
-    using namespace std;
+    std::vector<table_row> table = { { 1, "leo", "developer" },
+                                     { 2, "dima", "developer" },
+                                     { 3, "igor", "developer" } };
 
-    vector<om::table_row> table = { { 1, "leo", "developer" },
-                                    { 2, "dima", "developer" },
-                                    { 3, "igor", "developer" } };
+    auto it = std::ranges::find(table, "igor", &table_row::name);
 
-    auto it = ranges::find(table, "igor", &om::table_row::name);
-
-    cout << "uid: " << it->uid << '\n'
-         << "name: " << it->name << '\n'
-         << "info: " << it->info << '\n';
-    return cout.fail();
+    std::cout << "uid: " << it->uid << '\n'
+              << "name: " << it->name << '\n'
+              << "info: " << it->info << '\n';
+    return std::cout.fail();
 }
