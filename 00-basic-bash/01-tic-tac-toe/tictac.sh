909c4c4d2e6e17c56d065b3290e7fe6f767f86d8
leanid <leanid.chaika@gmail.com>
2024-04-07 23:44:24 +0300

    better tictac.sh bash example

--- 00-basic-bash/01-tic-tac-toe/tictac.sh
+++ 00-basic-bash/01-tic-tac-toe/tictac.sh
@@ -1,110 +1,103 @@
 #!/usr/bin/env bash
 
-echo "play XO-game"
+printf "%s\n" "play XO-game"
+# create array of XO-game board
+# it will create 9 variables from field0 ... to field8 with content '_'
+field=(_ _ _
+	_ _ _
+	_ _ _)
 
-field=(_ _ _ \
-       _ _ _ \
-       _ _ _ )
-
-function print_field()
-{
-    for ((i=0; i<3; i++))
-    do
-        echo "${field[((3*i))]}" "|" "${field[(((3*i)+1))]}" "|" "${field[(((3*i)+2))]}"
-    done
+function print_field() {
+	for ((i = 0; i < 3; i++)); do
+		# to do math expression surround with ((...))
+		local col0=${field[(((3 * i) + 0))]}
+		local col1=${field[(((3 * i) + 1))]}
+		local col2=${field[(((3 * i) + 2))]}
+		printf "%s|%s|%s\n" "$col0" "$col1" "$col2"
+	done
 }
 
-function check_3_indexes()
-{
-    i0=$1
-    i1=$2
-    i2=$3
+function check_3_indexes() {
+	local v0=${field[$1]}
+	local v1=${field[$2]}
+	local v2=${field[$3]}
 
-    v0=${field[((i0))]}
-    v1=${field[((i1))]}
-    v2=${field[((i2))]}
-
-    if [ "$v0" == "$v1" ] && [ "$v1" == "$v2" ]
-    then
-        if [ "$v0" == "X" ]
-        then
-            echo "X - won!!!"
-            return 0;
-        elif [ "$v0" == "O" ]
-        then
-            echo "O - won!!!"
-            return 0;
-        fi
-    fi
-    return 1;
-
+	if [ "$v0" == "$v1" ] && [ "$v1" == "$v2" ]; then
+		if [ "$v0" == "X" ]; then
+			echo "X - won!!!"
+			return 0
+		elif [ "$v0" == "O" ]; then
+			echo "O - won!!!"
+			return 0
+		fi
+	fi
+	return 1
 }
 
-function check_horizontal()
-{
-    i0=$1 # $1 - start index of horizontal line 0, 3, 6
-    i1=$((i0+1))
-    i2=$((i0+2))
+function check_horizontal() {
+	i0=$1 # $1 - start index of horizontal line 0, 3, 6
+	i1=$((i0 + 1))
+	i2=$((i0 + 2))
 
-    if check_3_indexes "$i0" "$i1" "$i2"
-    then
-        return 0
-    fi
-    return 1
+	if check_3_indexes "$i0" "$i1" "$i2"; then
+		return 0
+	fi
+	return 1
 }
 
-function check_vertical()
-{
-    i0=$1 # $1 - start index of vertical line 0, 1, 2
-    i1=$((i0+3))
-    i2=$((i0+6))
+function check_vertical() {
+	local i0=$1 # $1 - start index of vertical line 0, 1, 2
+	local i1=$((i0 + 3))
+	local i2=$((i0 + 6))
 
-    if check_3_indexes "$i0" "$i1" "$i2"
-    then
-        return 0
-    fi
-    return 1
+	if check_3_indexes "$i0" "$i1" "$i2"; then
+		return 0
+	fi
+	return 1
 }
 
-function check_all_horizontal()
-{
-    if check_horizontal 0 || check_horizontal 3 || check_horizontal 6
-    then
-        return 0;
-    fi
-    return 1;
+function check_all_horizontal() {
+	if check_horizontal 0 ||
+		check_horizontal 3 ||
+		check_horizontal 6; then
+		return 0
+	fi
+	return 1
 }
 
-function check_all_vertical()
-{
-    if check_vertical 0 || check_vertical 1 || check_vertical 2
-    then
-        return 0;
-    fi
-    return 1;
+function check_all_vertical() {
+	if check_vertical 0 ||
+		check_vertical 1 ||
+		check_vertical 2; then
+		return 0
+	fi
+	return 1
 }
 
-function check_win_state()
-{
-    if check_all_horizontal || check_all_vertical || check_3_indexes 0 4 8 || check_3_indexes 2 4 6
-    then
-        exit 0;
-    fi
-    return 1;
+function check_win_state() {
+	if check_all_horizontal ||
+		check_all_vertical ||
+		check_3_indexes 0 4 8 ||
+		check_3_indexes 2 4 6; then
+		exit 0
+	fi
+	return 1
 }
 
 print_field
 
-while true
-do
-    echo "Where to put X? (1-9)"
-    read -r position
-    field[((position-1))]="X"
-    print_field
-    check_win_state
-    echo "Where to put O? (1-9)"
-    read -r position
-    field[((position-1))]="O"
-    print_field
-    check_win_state
+while true; do
+	echo "Where to put X? (1-9)"
+	# Do not let backslash (\\) act as an escape character (read -r) - raw
+	read -r position
+	player1_pos=$((position - 1))
+	field[player1_pos]="X"
+	print_field
+	check_win_state
+	echo "Where to put O? (1-9)"
+	read -r position
+	player2_pos=$((position - 1))
+	field[player2_pos]="O"
+	print_field
+	check_win_state
 done
