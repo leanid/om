f7f38c90dccf041e97a15b6e272279c7887ca34c
leanid <leanid.chaika@gmail.com>
2024-04-11 00:09:30 +0300

    better security example

--- 00-basic-prog/26-security-error/main.cxx
+++ 00-basic-prog/26-security-error/main.cxx
@@ -2,6 +2,8 @@
 #include <cstdlib>
 #include <string>
 
+void help_how_to_hack_this_program(int argc, char** argv, char buf[]);
+
 int main(int argc, char** argv)
 {
     using namespace std;
@@ -12,18 +14,56 @@ int main(int argc, char** argv)
 
     std::printf("what is your name: ");
 
-    std::scanf("%1024s", sub_buf);
+    std::scanf("%1023s", sub_buf);
 
-    std::snprintf(buf, size, "echo \"your name is %s\"", sub_buf);
+    std::snprintf(buf, 1023, "echo \"your name is %s\"", sub_buf);
 
+    help_how_to_hack_this_program(argc, argv, buf);
+
+    std::system(buf);
+
+    return 0;
+}
+// just not see below, try first to think yourself
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+void help_how_to_hack_this_program(int argc, char** argv, char buf[])
+{
+    using namespace std;
     if (argc > 1 && (argv[1] == "--verbose"s || argv[1] == "-v"s))
     {
         // to "hack" this program one may input: "&&uname&&echo"
         std::printf("%s\n", buf);
         std::fflush(stdout);
     }
-
-    std::system(buf);
-
-    return 0;
 }
