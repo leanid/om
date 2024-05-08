85e7796fd0e0aac8a81111e7069f6c29bab04d55
Leanid Chaika <leanid.chaika@gmail.com>
2024-05-08 11:20:54 +0300

    prepare next example

---
+++ 08-web/02-fcgi-http/02_fcgi_http.cxx
@@ -0,0 +1,118 @@
+#include <functional>
+#include <iostream>
+#include <mutex>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <vector>
+
+#include <fcgiapp.h>
+
+namespace om
+{
+struct web_app_context
+{
+    std::string socket_path = "127.0.0.1:9000";
+    int         queue_size  = 20;
+    // хранит дескриптор открытого сокета
+    int        socket_id{};
+    std::mutex accept_mutex;
+};
+
+void worker_job(web_app_context& context)
+{
+    using namespace std::string_literals;
+
+    int          rc;
+    FCGX_Request request;
+    std::string  server_name;
+
+    if (FCGX_InitRequest(&request, context.socket_id, 0) != 0)
+    {
+        // ошибка при инициализации структуры запроса
+        std::cerr << "Can not init request\n";
+        return;
+    }
+    std::cout << "Request is inited\n";
+
+    auto accept_request = [](std::mutex&   accept_mutex,
+                             FCGX_Request& request) -> bool
+    {
+        int rc = FCGX_Accept_r(&request);
+        return rc >= 0;
+    };
+    // попробовать получить новый запрос
+    std::cout << "Try to accept new request" << std::endl;
+
+    while (accept_request(context.accept_mutex, request))
+    {
+        std::cout << "request is accepted" << std::endl;
+
+        std::stringstream out;
+
+        out << "Content-type: text/html\r\n"
+               "\r\n"
+               "<html>\r\n"
+               "    <head>\r\n"
+               "        <title>FastCGI Hello from C++!</title>\r\n"
+               "    </head>\r\n"
+               "    <body>\r\n"
+               "        <h1>FastCGI Hello! C++ fcgiapp</h1>\r\n"
+               "        <p>All Request params: <br/>\r\n";
+
+        for (char** current = request.envp; *current; current++)
+        {
+            // clang-format off
+        out << "            <i>" << *current << "</i><br/>\r\n";
+            // clang-format on
+        }
+
+        out << "        </p>\r\n"
+               "    </body>\r\n"
+               "</html>\r\n";
+        auto str = out.str();
+        FCGX_PutStr(str.data(), static_cast<int>(str.size()), request.out);
+
+        // закрыть текущее соединение
+        FCGX_Finish_r(&request);
+    }
+    std::cout << "fishish main loop" << std::endl;
+}
+
+} // namespace om
+
+int main(int argc, char** argv)
+{
+    om::web_app_context context;
+    size_t              num_of_cpu = 1; // std::thread::hardware_concurrency();
+    std::vector<std::jthread> threads;
+    threads.reserve(num_of_cpu);
+
+    // инициализация библилиотеки
+    if (0 != FCGX_Init())
+    {
+        std::cerr << "error: failed to FCGX_init()" << std::endl;
+        return 1;
+    }
+    std::cout << "FCGX_init is inited" << std::endl;
+
+    // открываем новый сокет
+    context.socket_id =
+        FCGX_OpenSocket(context.socket_path.c_str(), context.queue_size);
+    if (context.socket_id < 0)
+    {
+        // ошибка при открытии сокета
+        std::cerr << "error: failed to OpenSocket: " << context.socket_path
+                  << " error: " << context.socket_id << std::endl;
+        return 1;
+    }
+    std::cout << "Socket is opened" << std::endl;
+
+    // создаём рабочие потоки
+    while (num_of_cpu--)
+    {
+        threads.push_back(std::jthread(om::worker_job, std::ref(context)));
+    }
+
+    return 0;
+}
---
+++ 08-web/02-fcgi-http/readme.md
@@ -0,0 +1,56 @@
+# install nginx
+## on Ubuntu
+Go to https://nginx.org/ru/linux_packages.html#Ubuntu and do it
+# start stop nginx like:
+
+```bash
+# what is the status
+sudo systemctl status nginx
+# start
+sudo systemctl start nginx
+# stop
+sudo systemctl stop nginx
+# reload (reload configuration files)
+sudo systemctl reload nginx
+```
+# How to start with nginx (FastCGI)
+https://nginx.org/ru/docs/beginners_guide.html
+
+1. do:
+``` bash
+sudo vim /etc/nginx/conf.d/default.conf
+```
+end change `listen 80;` to `listen 81` (in case you can't use default 80 port)
+2. restart nginx
+``` bash
+# Fedora
+sudo services nginx restart
+# Ubuntu
+sudo systemctl restart nginx
+```
+3. go to you browser and open `localhost:81`. You should see default welcome nginx page.
+4. in case of truble check nginx configuration with `sudo nginx -t`
+
+
+# nginx problems
+1. check 80 port before start listening it (if using WSL check on windows too)
+   on Windows you can run Resource Monitor as Admin and tap Listening ports
+2. on Linux to see all listening ports use
+   `sudo netstat -tunlp`
+
+# minimal FastCGI nginx configuration
+
+``` nginx
+server { 
+    listen 82;
+    server_name localhost; 
+
+    location / { 
+        fastcgi_pass 127.0.0.1:9000; 
+        #fastcgi_pass  unix:/tmp/fastcgi/mysocket; 
+        #fastcgi_pass localhost:9000; 
+         
+        include fastcgi_params; 
+    } 
+}
+```
