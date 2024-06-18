845ded099762246ef0fa718e717bdb0e22bc1c4c
Leanid Chaika <leanid.chaika@gmail.com>
2024-06-18 10:40:08 +0300

    better om::files::content_t

--- 02-vulkan/07-vk-pipeline-1/files.hxx
+++ 02-vulkan/07-vk-pipeline-1/files.hxx
@@ -9,6 +9,26 @@ struct content_t
     std::unique_ptr<std::byte[]> memory;
     std::size_t                  size{};
 
+    content_t() noexcept
+        : memory{}
+        , size{}
+    {
+    }
+    content_t(content_t&& other) noexcept
+        : memory{ std::move(other.memory) }
+        , size{ other.size }
+    {
+        other.size = 0;
+    }
+
+    content_t& operator=(content_t&& other) noexcept
+    {
+        memory     = std::move(other.memory);
+        size       = other.size;
+        other.size = 0;
+        return *this;
+    }
+
     std::string_view as_string_view() const;
 };
 
