d1de81cd6328cb2e61c18130df16ea6af98ac925
leanid <leanid.chaika@gmail.com>
2024-06-30 16:14:45 +0300

    prepare for next tutorial

---
+++ 02-vulkan/08-vk-framebuffer-cmd/platform_sdl3.cxx
@@ -0,0 +1,78 @@
+#include "platform_sdl3.hxx"
+
+#include <utility>
+
+#include <SDL3/SDL_vulkan.h>
+
+#include "read_file.hxx"
+
+namespace om::vulkan
+{
+
+platform_interface::extensions platform_sdl3::get_vulkan_extensions()
+{
+    platform_interface::extensions extensions{};
+
+    extensions.names = SDL_Vulkan_GetInstanceExtensions(&extensions.count);
+
+    return extensions;
+}
+
+VkSurfaceKHR platform_sdl3::create_vulkan_surface(
+    VkInstance instance, VkAllocationCallbacks* alloc_callbacks)
+{
+    VkSurfaceKHR surface{};
+    SDL_bool     result =
+        SDL_Vulkan_CreateSurface(window, instance, alloc_callbacks, &surface);
+
+    if (!result)
+    {
+        throw std::runtime_error(SDL_GetError());
+    }
+
+    return surface;
+}
+
+void platform_sdl3::destroy_vulkan_surface(
+    VkInstance             instance,
+    VkSurfaceKHR           surface,
+    VkAllocationCallbacks* alloc_callbacks)
+{
+    SDL_Vulkan_DestroySurface(instance, surface, alloc_callbacks);
+}
+
+platform_interface::buffer_size platform_sdl3::get_window_buffer_size()
+{
+    platform_interface::buffer_size buffer_size{};
+
+    int w{};
+    int h{};
+
+    if (0 != SDL_GetWindowSizeInPixels(window, &w, &h))
+    {
+        throw std::runtime_error(SDL_GetError());
+    }
+
+    buffer_size.width  = static_cast<uint32_t>(w);
+    buffer_size.height = static_cast<uint32_t>(h);
+
+    return buffer_size;
+}
+
+std::ostream& platform_sdl3::get_logger()
+{
+    return log;
+}
+
+platform_interface::content platform_sdl3::get_file_content(
+    std::string_view path)
+{
+    content     result{};
+    io::content content = io::read_file(path);
+
+    result.memory = std::move(content.memory);
+    result.size   = std::exchange(content.size, 0);
+
+    return result;
+}
+} // namespace om::vulkan
---
+++ 02-vulkan/08-vk-framebuffer-cmd/render.hxx
@@ -0,0 +1,189 @@
+#pragma once
+
+#include <iosfwd>
+#include <limits>
+#include <memory> // std::unique_ptr
+#include <span>
+#include <string_view>
+#include <utility> // std::exchange
+#include <vector>
+
+#include <experimental/scope> // not found on macOS
+
+#include <vulkan/vulkan.hpp>
+
+namespace om::vulkan
+{
+struct platform_interface
+{
+    virtual ~platform_interface() = default;
+
+    struct extensions
+    {
+        const char* const* names = nullptr;
+        std::uint32_t      count = 0u;
+    };
+    struct buffer_size
+    {
+        std::uint32_t width  = 0u;
+        std::uint32_t height = 0u;
+    };
+    struct content
+    {
+        std::unique_ptr<std::byte[]> memory;
+        std::size_t                  size{};
+
+        content(const content& other)            = delete;
+        content& operator=(const content& other) = delete;
+
+        content() noexcept
+            : memory{}
+            , size{}
+        {
+        }
+        content(content&& other) noexcept
+            : memory{ std::move(other.memory) }
+            , size{ std::exchange(other.size, 0) }
+        {
+        }
+
+        content& operator=(content&& other) noexcept
+        {
+            memory = std::move(other.memory);
+            size   = std::exchange(other.size, 0);
+            return *this;
+        }
+
+        [[nodiscard]] std::span<std::byte> as_span() const noexcept
+        {
+            return std::span{ memory.get(), size };
+        }
+    };
+
+    virtual extensions   get_vulkan_extensions() = 0;
+    virtual VkSurfaceKHR create_vulkan_surface(
+        VkInstance instance, VkAllocationCallbacks* alloc_callbacks) = 0;
+    virtual void destroy_vulkan_surface(
+        VkInstance             instance,
+        VkSurfaceKHR           surface,
+        VkAllocationCallbacks* alloc_callbacks)  = 0;
+    virtual buffer_size get_window_buffer_size() = 0;
+
+    virtual std::ostream& get_logger() = 0;
+
+    virtual content get_file_content(std::string_view path) = 0;
+};
+
+class render
+{
+public:
+    struct hints
+    {
+        bool verbose;
+        bool enable_validation_layers;
+    };
+
+    explicit render(platform_interface& platform, hints hints);
+
+    ~render();
+
+private:
+    struct swapchain_details_t
+    {
+        vk::SurfaceCapabilitiesKHR        surface_capabilities;
+        std::vector<vk::SurfaceFormatKHR> surface_formats;
+        std::vector<vk::PresentModeKHR>   presentation_modes;
+    };
+
+    void create_instance();
+    void create_logical_device();
+    void create_surface();
+    void create_swapchain();
+    void create_renderpass();
+    void create_graphics_pipeline();
+
+    [[nodiscard]] vk::ImageView create_image_view(
+        vk::Image            image,
+        vk::Format           format,
+        vk::ImageAspectFlags aspect_flags) const;
+
+    vk::ShaderModule create_shader(std::span<std::byte> spir_v);
+
+    void destroy_surface();
+    void destroy(vk::ShaderModule& shader);
+
+    void validate_expected_extensions_exists(
+        const vk::InstanceCreateInfo& create_info);
+    void validate_instance_layer_present(std::string_view instance_layer);
+    void validate_physical_device();
+
+    bool        check_device_suitable(vk::PhysicalDevice& physical);
+    static bool check_device_extension_supported(
+        vk::PhysicalDevice& device, std::string_view extension_name);
+
+    void                get_physical_device();
+    swapchain_details_t get_swapchain_details(vk::PhysicalDevice& device);
+
+    static uint32_t get_render_queue_family_index(
+        const vk::PhysicalDevice& physical_device);
+    static uint32_t get_presentation_queue_family_index(
+        const vk::PhysicalDevice& physical_device,
+        const vk::SurfaceKHR&     surface_to_check);
+
+    vk::Extent2D choose_best_swapchain_image_resolution(
+        const vk::SurfaceCapabilitiesKHR& capabilities);
+    static vk::SurfaceFormatKHR choose_best_surface_format(
+        std::span<vk::SurfaceFormatKHR> formats);
+    static vk::PresentModeKHR choose_best_present_mode(
+        std::span<vk::PresentModeKHR> present_modes);
+
+    friend std::ostream& operator<<(std::ostream&              os,
+                                    const swapchain_details_t& details);
+
+    // render external interface objects
+    std::ostream&       log;
+    platform_interface& platform_;
+    hints               hints_;
+
+    // vulkan main objects
+    vk::Instance instance;
+
+    struct
+    {
+        vk::PhysicalDevice physical;
+        vk::Device         logical;
+    } devices;
+
+    [[maybe_unused]] vk::Queue render_queue;
+    [[maybe_unused]] vk::Queue presentation_queue;
+    vk::SurfaceKHR             surface; // KHR - extension
+    vk::SwapchainKHR           swapchain;
+    std::vector<vk::Image>     swapchain_images;
+    std::vector<vk::ImageView> swapchain_image_views;
+
+    // vulkan pipeline
+    vk::Pipeline       graphics_pipeline{};
+    vk::PipelineLayout pipeline_layout{};
+    vk::RenderPass     render_path;
+
+    // vulkan utilities
+    vk::Format   swapchain_image_format{};
+    vk::Extent2D swapchain_image_extent{};
+
+    struct queue_family_indexes
+    {
+        uint32_t graphics_family     = std::numeric_limits<uint32_t>::max();
+        uint32_t presentation_family = std::numeric_limits<uint32_t>::max();
+
+        [[nodiscard]] bool is_valid() const
+        {
+            return graphics_family != std::numeric_limits<uint32_t>::max() &&
+                   presentation_family != std::numeric_limits<uint32_t>::max();
+        }
+    } queue_indexes;
+
+    const std::vector<const char*> device_extensions{
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME
+    };
+};
+} // namespace om::vulkan
