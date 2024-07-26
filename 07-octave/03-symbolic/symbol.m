64a63258297aabc4423e760051c4b2c55744192a
leanid <leanid.chaika@gmail.com>
2024-07-26 23:41:48 +0300

    add example for two point collision

--- 07-octave/03-symbolic/symbol.m
+++ 07-octave/03-symbolic/symbol.m
@@ -19,3 +19,17 @@ eq1 = x + y == 6;
 eq2 = x - y == 2;
 ans1 = solve(eq1, eq2);
 fprintf("x = %f y = %f\n", double(ans1.x), double(ans1.y));
+
+syms m1 real;
+syms v1 real;
+syms m2 real;
+syms v2 real;
+syms v3 real;
+syms v4 real;
+eq3 = m1*v1 + m2*v2 == m1*v3 + m2*v4;
+eq4 = (m1*v1^2)*0.5 + (m2*v2^2)*0.5 == (m1*v3^2)*0.5 + (m2*v4^2)*0.5;
+ans2 = solve(eq3,eq4, v3, v4);
+disp(ans2{1}.v3);
+disp(ans2{1}.v4);
+disp(ans2{2}.v3);
+disp(ans2{2}.v4);
