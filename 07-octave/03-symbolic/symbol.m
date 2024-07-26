b7d3fce5b9a029835f0c70f7a9cf63eed929c1c7
leanid <leanid.chaika@gmail.com>
2024-07-26 22:59:33 +0300

    add example of solving system of linear equations

--- 07-octave/03-symbolic/symbol.m
+++ 07-octave/03-symbolic/symbol.m
@@ -6,7 +6,16 @@
 % > pip install --user sympy (check pip --version same as default python)
 pkg load symbolic
 
-syms a b c x;
+syms a b c x y;
 f = a*x+b+c;
 
 solve(f==0,x) % expected result: (-b - c)/a
+
+f(x) = 3*x^2+2*x+5;
+f(1) % expected result: 10
+
+% lets solve system of linear equations
+eq1 = x + y == 6;
+eq2 = x - y == 2;
+ans1 = solve(eq1, eq2);
+fprintf("x = %f y = %f\n", double(ans1.x), double(ans1.y));
