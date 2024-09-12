e401bd296508e7eb57be1ad226162f91b4792a1e
Leanid Chaika <leanid.chaika@gmail.com>
2024-09-12 12:07:54 +0300

    works on MSVC-2022 with Ninja generator

--- 00-basic-prog/12-modules/main.cxx
+++ 00-basic-prog/12-modules/main.cxx
@@ -1,4 +1,4 @@
-// this works on MSVC2022
+// this works on MSVC2022 with Ninja generator
 import std;
 import hello;
 
@@ -7,5 +7,11 @@ int main(void)
     std::cout << "printing some text\n";
     ask::greeter("world");
     std::println("print with std::println");
+    auto user = std::getenv("USER");
+    if (!user)
+    {
+        user = std::getenv("USERNAME");
+    }
+    std::println("your name is {}", user);
     return std::cout.fail();
 }
