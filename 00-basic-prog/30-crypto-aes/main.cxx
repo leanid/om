803fe939f861fc281a321924e33311594016d36b
Leanid Chaika <leanid.chaika@gmail.com>
2024-10-14 16:07:20 +0300

    encode-decode works!

--- 00-basic-prog/30-crypto-aes/main.cxx
+++ 00-basic-prog/30-crypto-aes/main.cxx
@@ -124,19 +124,127 @@ void encrypt(const std::string& in_file,
     ofs.write(reinterpret_cast<const char*>(cipherBuffer.data()), outLen);
 }
 
+void decrypt(const std::string& in_file,
+             const std::string& out_file,
+             const std::string& password)
+{
+    const int  key_len    = 16;         // AES-128
+    const int  iv_len     = 16;         // AES-CTR IV length
+    const int  iterations = 10000;      // see: PBKDF2_ITER_DEFAULT 10000
+    const int  salt_len   = 8;          // default like in openssl
+    const char magic[]    = "Salted__"; // default like in openssl
+
+    std::ifstream ifs(in_file, std::ios::binary);
+    if (!ifs)
+    {
+        std::string msg("Error opening input file: ");
+        msg += in_file;
+        throw std::runtime_error(msg);
+    }
+
+    char file_magic[sizeof(magic) - 1]{};
+    ifs.read(file_magic, sizeof(file_magic));
+    if (std::memcmp(file_magic, magic, sizeof(magic) - 1) != 0)
+    {
+        throw std::runtime_error("Bad magic constant");
+    }
+
+    unsigned char salt[salt_len];
+    ifs.read(reinterpret_cast<char*>(salt), salt_len);
+
+    unsigned char key_and_iv[key_len + iv_len];
+    if (!PKCS5_PBKDF2_HMAC(password.c_str(),
+                           static_cast<int>(password.length()),
+                           salt,
+                           salt_len,
+                           iterations,
+                           EVP_sha256(),
+                           key_len + iv_len,
+                           key_and_iv))
+    {
+        throw_errors();
+    }
+
+    unsigned char key[key_len];
+    unsigned char iv[iv_len];
+
+    std::memcpy(key, key_and_iv, key_len);
+    std::memcpy(iv, key_and_iv + key_len, iv_len);
+
+    std::shared_ptr<EVP_CIPHER_CTX> ctx(EVP_CIPHER_CTX_new(),
+                                        EVP_CIPHER_CTX_free);
+    if (!ctx)
+    {
+        throw_errors();
+    }
+
+    if (1 != EVP_DecryptInit_ex(ctx.get(), EVP_aes_128_ctr(), nullptr, key, iv))
+    {
+        throw_errors();
+    }
+
+    std::ofstream ofs(out_file, std::ios::binary);
+    if (!ofs)
+    {
+        std::string msg("Error opening output file: ");
+        msg += out_file;
+        throw std::runtime_error(msg);
+    }
+
+    std::vector<unsigned char> buffer(1024);
+    std::vector<unsigned char> plainBuffer(
+        1024 + EVP_CIPHER_block_size(EVP_aes_128_ctr()));
+    int outLen = 0;
+
+    ifs.read(reinterpret_cast<char*>(buffer.data()),
+             static_cast<std::streamsize>(buffer.size()));
+
+    while (ifs.gcount())
+    {
+        if (1 != EVP_DecryptUpdate(ctx.get(),
+                                   plainBuffer.data(),
+                                   &outLen,
+                                   buffer.data(),
+                                   static_cast<int>(ifs.gcount())))
+        {
+            throw_errors();
+        }
+        ofs.write(reinterpret_cast<const char*>(plainBuffer.data()), outLen);
+        ifs.read(reinterpret_cast<char*>(buffer.data()),
+                 static_cast<std::streamsize>(buffer.size()));
+    }
+
+    if (1 != EVP_DecryptFinal_ex(ctx.get(), plainBuffer.data(), &outLen))
+    {
+        throw_errors();
+    }
+    ofs.write(reinterpret_cast<const char*>(plainBuffer.data()), outLen);
+}
+
 int main()
 {
-    const std::string password = "leanid";
-    const std::string in_file  = "ru.yaml";
-    const std::string out_file = "ru.yaml.enc";
+    const std::string password     = "leanid";
+    const std::string in_file      = "ru.yaml";
+    const std::string out_file_enc = "ru.yaml.enc";
+    const std::string out_file_dec = "ru.yaml.enc.dec";
 
     try
     {
-        encrypt(in_file, out_file, password);
+        encrypt(in_file, out_file_enc, password);
     }
     catch (const std::exception& ex)
     {
         std::cerr << "error: encrypt failed: " << ex.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    try
+    {
+        decrypt(out_file_enc, out_file_dec, password);
+    }
+    catch (const std::exception& ex)
+    {
+        std::cerr << "error: decrypt failed: " << ex.what() << std::endl;
         return EXIT_FAILURE;
     }
 
