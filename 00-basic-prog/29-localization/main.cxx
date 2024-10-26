6cf8650194a83d9a17d62f9ef82780c389c960b5
leanid <leanid.chaika@gmail.com>
2024-10-26 22:38:18 +0300

    add one more boost locale example

--- 00-basic-prog/29-localization/main.cxx
+++ 00-basic-prog/29-localization/main.cxx
@@ -147,10 +147,71 @@ void boost_locale_conversion_example()
                  "limiting the correct behavior to BMP or ASCII only\n";
 }
 
+void boost_locale_boundary_example()
+{
+    using namespace boost::locale;
+
+    generator gen;
+    // Make system default locale global
+    std::locale loc = gen("");
+    // We need the boundary facet, currently only available via ICU
+    if (!std::has_facet<boundary::boundary_indexing<char>>(loc))
+    {
+        std::cout << "boundary detection not implemented in this environment\n";
+        return;
+    }
+    std::locale::global(loc);
+    std::cout.imbue(loc);
+
+    std::string text = "Hello World! あにま! Linux2.6 and Windows7 is word and "
+                       "number. שָלוֹם עוֹלָם!";
+
+    std::cout << text << std::endl;
+
+    boundary::ssegment_index index(boundary::word, text.begin(), text.end());
+
+    for (boundary::ssegment_index::iterator p = index.begin(), e = index.end();
+         p != e;
+         ++p)
+    {
+        std::cout << "Part [" << *p << "] has ";
+        if (p->rule() & boundary::word_number)
+            std::cout << "number(s) ";
+        if (p->rule() & boundary::word_letter)
+            std::cout << "letter(s) ";
+        if (p->rule() & boundary::word_kana)
+            std::cout << "kana character(s) ";
+        if (p->rule() & boundary::word_ideo)
+            std::cout << "ideographic character(s) ";
+        if (p->rule() & boundary::word_none)
+            std::cout << "no word characters";
+        std::cout << std::endl;
+    }
+
+    index.map(boundary::character, text.begin(), text.end());
+
+    for (const boundary::ssegment& p : index)
+        std::cout << "|" << p;
+    std::cout << "|\n\n";
+
+    index.map(boundary::line, text.begin(), text.end());
+
+    for (const boundary::ssegment& p : index)
+        std::cout << "|" << p;
+    std::cout << "|\n\n";
+
+    index.map(boundary::sentence, text.begin(), text.end());
+
+    for (const boundary::ssegment& p : index)
+        std::cout << "|" << p;
+    std::cout << "|\n\n";
+}
+
 int main()
 {
     boost_locale_hello_example();
     boost_locale_conversion_example();
+    boost_locale_boundary_example();
 
     using namespace std;
     std::locale default_cxx = locale("");
