9101e64c6e54cfecba2bc65b11a706dcef8d286a
leanid <leanid.chaika@gmail.com>
2024-11-03 01:47:34 +0300

    minor update

--- 09-elisp/01-hello/01-hello.el
+++ 09-elisp/01-hello/01-hello.el
@@ -1,3 +1,6 @@
 ;; to execute next line in doomemacs
 ;; set pointer on it and press: SPC+m+e+d
 (defun hello () "hello world!")
+;; to execute call to newlly created function
+;; set pointer to last ')' and press: SPC+m+e+e
+(hello)
