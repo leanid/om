239168827a251d5bd7c7482c42f384a56e957937
leanid chaika <leanid.chaika@gmail.com>
2024-11-03 04:00:12 +0300

    add comments to elisp

--- 09-elisp/01-hello/01-hello.el
+++ 09-elisp/01-hello/01-hello.el
@@ -1,6 +1,10 @@
-;; to execute next line in doomemacs
+;;; package --- Summary
+;;; Commentary:
+;;; hello To execute next line in doomemacs
 ;; set pointer on it and press: SPC+m+e+d
+;;; Code:
 (defun hello () "hello world!")
 ;; to execute call to newlly created function
 ;; set pointer to last ')' and press: SPC+m+e+e
 (hello)
+;;; 01-hello.el ends here
