0c1bebb43501c1c3a7beb9bf7e24bf116340675f
Leanid Chaika <leanid.chaika@gmail.com>
2024-12-23 18:01:21 +0300

    change naming for simplisity

--- 00-basic-prog/36-gtest/main.cxx
+++ 00-basic-prog/36-gtest/main.cxx
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 
 // Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions)
+TEST(gtest_36, basic_assertions)
 {
     // Expect two strings not to be equal.
     EXPECT_STRNE("hello", "world");
