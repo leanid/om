f0fb78e63771a827ac4714cbdd4002bc43f6d923
Leanid Chaika <leanid.chaika@gmail.com>
2024-12-26 12:23:25 +0300

    make first test example same for gtest

--- 00-basic-prog/36-gtest/main.cxx
+++ 00-basic-prog/36-gtest/main.cxx
@@ -1,10 +1,32 @@
 #include <gtest/gtest.h>
 
-// Demonstrate some basic assertions.
-TEST(gtest_36, basic_assertions)
+class gtest_36 : public testing::Test
 {
-    // Expect two strings not to be equal.
-    EXPECT_STRNE("hello", "world");
-    // Expect equality.
-    EXPECT_EQ(7 * 6, 42);
+protected:
+    gtest_36()
+        : v(5)
+    {
+        // "vectors can be sized and resized"
+        EXPECT_EQ(v.size(), 5);
+        EXPECT_GE(v.capacity(), 5);
+    }
+
+    std::vector<int> v;
+};
+
+// adding to the vector increases its size
+TEST_F(gtest_36, adding_to_the_vector_increases_its_size)
+{
+    v.push_back(1);
+
+    EXPECT_EQ(v.size(), 6);
+    EXPECT_GE(v.capacity(), 6);
+}
+
+TEST_F(gtest_36, reserving_increases_just_the_capacity)
+{
+    v.reserve(6);
+
+    EXPECT_EQ(v.size(), 5);
+    EXPECT_GE(v.capacity(), 6);
 }
