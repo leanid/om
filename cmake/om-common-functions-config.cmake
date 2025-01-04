30efe9349c83699e6b713002f873070401236df2
leanid <leanid.chaika@gmail.com>
2025-01-04 21:52:31 +0300

    add example how to add custom build type

---
+++ cmake/om-common-functions-config.cmake
@@ -0,0 +1,29 @@
+cmake_minimum_required(VERSION 3.31)
+
+# just right after top project(..) command
+# you may add custom build types
+function (om_add_custom_build_types)
+    # Only modify config details if the top level project
+    if(CMAKE_SOURCE_DIR STREQUAL CMAKE_CURRENT_SOURCE_DIR)
+        get_property(is_multi_config GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
+        if(NOT is_multi_config)
+            set_property(CACHE CMAKE_BUILD_TYPE PROPERTY
+                STRINGS Debug Release RelWithDebInfo MinSizeRel Profile
+            )
+        elseif(NOT "Profile" IN_LIST CMAKE_CONFIGURATION_TYPES)
+            list(APPEND CMAKE_CONFIGURATION_TYPES Profile)
+        endif()
+        # Set Profile-specific flag variables (see below)...
+        set(CMAKE_C_FLAGS_PROFILE "${CMAKE_C_FLAGS_RELWITHDEBINFO} -p")
+        set(CMAKE_CXX_FLAGS_PROFILE "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} -p")
+        set(CMAKE_EXE_LINKER_FLAGS_PROFILE "${CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO} -p")
+        set(CMAKE_SHARED_LINKER_FLAGS_PROFILE "${CMAKE_SHARED_LINKER_FLAGS_RELWITHDEBINFO} -p")
+        set(CMAKE_STATIC_LINKER_FLAGS_PROFILE "${CMAKE_STATIC_LINKER_FLAGS_RELWITHDEBINFO}")
+        set(CMAKE_MODULE_LINKER_FLAGS_PROFILE "${CMAKE_MODULE_LINKER_FLAGS_RELWITHDEBINFO} -p")
+
+        set(CMAKE_PROFILE_POSTFIX _profile)
+    else()
+        message(FATAL_ERROR "only in top level CMakeLists.txt after project(...)"
+                            " command you should add your custom build types")
+    endif()
+endfunction()
