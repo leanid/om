35546b19116767bd57ff104c3eb18d9887f4fb33
leanid <leanid.chaika@gmail.com>
2025-02-15 10:28:21 +0300

    try to use import glm;

--- 00-basic-prog/38-import-cxx/main.cxx
+++ 00-basic-prog/38-import-cxx/main.cxx
@@ -3,7 +3,7 @@ import glm;
 
 int main()
 {
-    glm::mat4 m = glm::translate(glm::mat4(1.0f), glm::vec3(1.0f));
+    glm::mat4 m{}; // = glm::translate(glm::mat4(1.0f), glm::vec3(1.0f));
     std::cout << "print from import glm;" << std::endl;
     std::cout << m[0][0] << std::endl;
     return std::cout.fail();
