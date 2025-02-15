113146c1e0a9288aef25aa1a16ca45851d8efd17
leanid <leanid.chaika@gmail.com>
2025-02-15 10:52:02 +0300

    add some glm examples it's works

--- 00-basic-prog/38-import-cxx/main.cxx
+++ 00-basic-prog/38-import-cxx/main.cxx
@@ -3,8 +3,15 @@ import glm;
 
 int main()
 {
-    glm::mat4 m{}; // = glm::translate(glm::mat4(1.0f), glm::vec3(1.0f));
+    using namespace std::numbers;
+    glm::mat4 m{ 1.f }; // = glm::translate(glm::mat4(1.0f), glm::vec3(1.0f));
+    glm::vec3 v{ 1.f };
+    float     up  = glm::degrees(pi);
+    float     pii = glm::radians(up);
     std::cout << "print from import glm;" << std::endl;
     std::cout << m[0][0] << std::endl;
+    std::cout << v[0] << std::endl;
+    std::cout << up << std::endl;
+    std::cout << pii << std::endl;
     return std::cout.fail();
 }
