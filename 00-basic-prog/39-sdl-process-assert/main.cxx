57cefdf3cb39cfcbb19b90de8a96e6867d201a70
leanid <leanid.chaika@gmail.com>
2025-02-28 23:19:32 +0100

    not working but intresting

--- 00-basic-prog/39-sdl-process-assert/main.cxx
+++ 00-basic-prog/39-sdl-process-assert/main.cxx
@@ -3,14 +3,70 @@
 
 #include "show_message.hxx"
 
+#include <boost/program_options.hpp>
+
 #include <cstdlib>
 #include <iostream>
+#include <string_view>
 
 int main(int argc, char** argv)
 {
 #if !__cpp_lib_span_initializer_list
 #error "need __cpp_lib_span_initializer_list"
 #endif
+
+    namespace po = boost::program_options;
+    po::options_description help("help");
+    help.add_options()("help,v", "print this help");
+
+    po::options_description message_options("message options");
+    message_options.add_options()("help", "print usage");
+    message_options.add_options()("button",
+                                  po::value<std::vector<std::string>>(),
+                                  "multiple buttons names, order is important");
+    message_options.add_options()(
+        "title", po::value<std::string>(), "title utf8 string value");
+    message_options.add_options()(
+        "text", po::value<std::string>(), "text utf8 message");
+
+    po::command_line_parser parser{ argc, argv };
+    parser.options(message_options);
+    po::parsed_options parsed_options = parser.run();
+
+    po::variables_map vm;
+    po::store(parsed_options, vm);
+
+    if (vm.count("help"))
+    {
+        std::cout << message_options << std::endl;
+        return EXIT_SUCCESS;
+    }
+
+    std::vector<std::u8string> buttons_in;
+    if (vm.count("button"))
+    {
+        buttons_in = vm["button"].as<std::vector<std::u8string>>();
+    }
+
+    std::u8string text;
+    if (vm.count("text"))
+    {
+        text = vm["text"].as<std::u8string>();
+    }
+
+    std::u8string title;
+    if (vm.count("title"))
+    {
+        title = vm["title"].as<std::u8string>();
+    }
+
+    if (!text.empty())
+    {
+        om::show_message(
+            title,
+            text,
+            std::span<std::u8string>(buttons_in.begin(), buttons_in.end()));
+    }
 
     std::array<std::u8string_view, 4> buttons = {
         u8"да", u8"отмена", u8"далее", u8"стоп"
