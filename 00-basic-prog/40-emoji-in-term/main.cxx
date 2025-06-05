9098b2498d639d238af1563d0f2bc5ce2e67e0cd
Leanid Chaika <leanid.chaika@gmail.com>
2025-06-05 16:37:30 +0300

    add utf-8 terminal emoji example

---
+++ 00-basic-prog/40-emoji-in-term/main.cxx
@@ -0,0 +1,42 @@
+#include <iomanip>
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    std::cout << "Можем ли мы писать по русски и использовать Эмоджи?\n";
+    std::cout
+        << "✅ - check mark button (code: U+2705)\n"
+        << "❌ - cross mark (code: U+274C)\n"
+        << "🇧🇾 - flag: Belarus (code: U+1F1E7-1F1FE)\n"
+        << "🇺🇸 - flag: United States (code: U+1F1FA-1F1F8)\n"
+        << "👨‍👩‍👧 - man, woman, girl (code: U+1F468 man,U+200D "
+           "zero width "
+           "joiner,U+1F469 woman, U+200D,U+1F467 girl)\n";
+    std::string check   = "✅";
+    std::string cross   = "❌";
+    std::string flag_by = "🇧🇾";
+    std::string flag_us = "🇺🇸";
+    std::string family  = "👨‍👩‍👧";
+
+    auto list = { check, cross, flag_by, flag_us, family };
+    for (auto emoji : list)
+    {
+        std::cout << "emoji [" << emoji
+                  << "] in utf-8 (ascii chars) length: " << emoji.length()
+                  << "\n";
+    }
+
+    for (auto emoji : list)
+    {
+        std::cout << "[" << emoji << "] chars = {";
+
+        for (char delim{ ' ' }; char ch : emoji)
+        {
+            std::cout << std::exchange(delim, ',') << "0x" << std::hex
+                      << std::setw(2) << static_cast<uint32_t>(ch);
+        }
+        std::cout << "}\n";
+    }
+
+    return EXIT_SUCCESS;
+}
