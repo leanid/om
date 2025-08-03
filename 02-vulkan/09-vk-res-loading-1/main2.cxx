a0c6dcc16f8c774814d5fa6f62b140c6fc56cc93
leanid <leanid.chaika@gmail.com>
2025-08-03 13:42:56 +0300

    import vulkan_hpp work with linking

--- 02-vulkan/09-vk-res-loading-1/main2.cxx
+++ 02-vulkan/09-vk-res-loading-1/main2.cxx
@@ -1,17 +1,25 @@
 import std;
 import vulkan_hpp;
 
+#define VK_VERSION_MAJOR(version) (((std::uint32_t)(version) >> 22U) & 0x7FU)
+#define VK_VERSION_MINOR(version) (((std::uint32_t)(version) >> 12U) & 0x3FFU)
+#define VK_VERSION_PATCH(version) ((std::uint32_t)(version) & 0xFFFU)
+
 int main(int argc, char** argv)
 {
     try
     {
         std::println("am I dreaming or it's works!");
+        std::cout << "hello from main2.cxx\n";
+        std::uint32_t maximum_supported = vk::enumerateInstanceVersion();
+        std::cout << "vk api version: " << VK_VERSION_MAJOR(maximum_supported)
+                  << '.' << VK_VERSION_MINOR(maximum_supported) << '.'
+                  << VK_VERSION_PATCH(maximum_supported)
+                  << " maximum supported\n";
     }
     catch (std::exception& e)
     {
         std::cerr << e.what();
     }
-    std::cout << "hello from main2.cxx\n";
-    std::cout << "sizeof(vk::PhysicalDevice): " << sizeof(vk::PhysicalDevice)
-              << std::endl;
+    return 0;
 }
