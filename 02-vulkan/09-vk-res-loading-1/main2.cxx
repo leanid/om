42f9a6476e1806e7a3b9621468e3624c51ce459d
leanid <leanid.chaika@gmail.com>
2025-08-03 14:50:19 +0300

    import glm; works!

--- 02-vulkan/09-vk-res-loading-1/main2.cxx
+++ 02-vulkan/09-vk-res-loading-1/main2.cxx
@@ -1,9 +1,8 @@
 import std;
 import vulkan_hpp;
+import glm;
 
-#define VK_VERSION_MAJOR(version) (((std::uint32_t)(version) >> 22U) & 0x7FU)
-#define VK_VERSION_MINOR(version) (((std::uint32_t)(version) >> 12U) & 0x3FFU)
-#define VK_VERSION_PATCH(version) ((std::uint32_t)(version) & 0xFFFU)
+std::string to_version(std::uint32_t v);
 
 int main(int argc, char** argv)
 {
@@ -12,14 +11,34 @@ int main(int argc, char** argv)
         std::println("am I dreaming or it's works!");
         std::cout << "hello from main2.cxx\n";
         std::uint32_t maximum_supported = vk::enumerateInstanceVersion();
-        std::cout << "vk api version: " << VK_VERSION_MAJOR(maximum_supported)
-                  << '.' << VK_VERSION_MINOR(maximum_supported) << '.'
-                  << VK_VERSION_PATCH(maximum_supported)
+        std::cout << "vk api version: " << to_version(maximum_supported)
                   << " maximum supported\n";
+        glm::mat4 m{ 1.f };
+        glm::vec3 v{ 1.f };
+
+        float     up   = glm::degrees(std::numbers::pi / 2.f);
+        float     pi_2 = glm::radians(up);
+        glm::mat4 mv   = glm::translate(m, v);
+
+        std::cout << "print from import glm;" << std::endl;
+        std::cout << m[0][0] << std::endl;
+        std::cout << mv[3][0] << std::endl;
+        std::cout << v[0] << std::endl;
+        std::cout << up << std::endl;
+        std::cout << pi_2 << std::endl;
     }
     catch (std::exception& e)
     {
         std::cerr << e.what();
     }
     return 0;
+}
+
+std::string to_version(std::uint32_t v)
+{
+    auto major = (((std::uint32_t)(v) >> 22U) & 0x7FU);
+    auto minor = (((std::uint32_t)(v) >> 12U) & 0x3FFU);
+    auto patch = ((std::uint32_t)(v) & 0xFFFU);
+    return std::to_string(major) + '.' + std::to_string(minor) + '.' +
+           std::to_string(patch);
 }
