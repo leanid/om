9c8bec27f2addfded883f167c9b94a8102787021
leanid <leanid.chaika@gmail.com>
2025-08-03 13:08:42 +0300

    import vulkan_hpp works! on Fedora with llvm

--- 02-vulkan/09-vk-res-loading-1/main2.cxx
+++ 02-vulkan/09-vk-res-loading-1/main2.cxx
@@ -1,6 +1,17 @@
 import std;
+import vulkan_hpp;
 
 int main(int argc, char** argv)
 {
+    try
+    {
+        std::println("am I dreaming or it's works!");
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what();
+    }
     std::cout << "hello from main2.cxx\n";
+    std::cout << "sizeof(vk::PhysicalDevice): " << sizeof(vk::PhysicalDevice)
+              << std::endl;
 }
