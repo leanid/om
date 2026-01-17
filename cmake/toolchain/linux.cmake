92a51cc194a3dc097b915292903c74aac6b529b1
leanid.chaika <leanid.chaika@gmail.com>
2026-01-17 22:45:53 +0300

    It WORKS!

--- cmake/toolchain/linux.cmake
+++ cmake/toolchain/linux.cmake
@@ -2,3 +2,4 @@
 # objects and libraries path for linker to find crt on current system
 set(CMAKE_EXE_LINKER_FLAGS "-L/usr/lib64/gcc/x86_64-alt-linux/13/ -B/usr/lib64/gcc/x86_64-alt-linux/13/")
 set(CMAKE_SHARED_LINKER_FLAGS "-L/usr/lib64/gcc/x86_64-alt-linux/13/ -B/usr/lib64/gcc/x86_64-alt-linux/13/")
+set(CMAKE_MODULE_LINKER_FLAGS "-L/usr/lib64/gcc/x86_64-alt-linux/13/ -B/usr/lib64/gcc/x86_64-alt-linux/13/")
