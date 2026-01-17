10085c91e1f7e545819e296a7bdbc20797a221c5
leanid.chaika <leanid.chaika@gmail.com>
2026-01-17 23:53:22 +0300

    use toolchain file

--- cmake/toolchain/linux.cmake
+++ cmake/toolchain/linux.cmake
@@ -1,5 +1,25 @@
 # on Alt Linux to build with downloaded latest llvm we need to pass search path for
 # objects and libraries path for linker to find crt on current system
-set(CMAKE_EXE_LINKER_FLAGS "-L/usr/lib64/gcc/x86_64-alt-linux/13/ -B/usr/lib64/gcc/x86_64-alt-linux/13/")
-set(CMAKE_SHARED_LINKER_FLAGS "-L/usr/lib64/gcc/x86_64-alt-linux/13/ -B/usr/lib64/gcc/x86_64-alt-linux/13/")
-set(CMAKE_MODULE_LINKER_FLAGS "-L/usr/lib64/gcc/x86_64-alt-linux/13/ -B/usr/lib64/gcc/x86_64-alt-linux/13/")
+# 1. Ask the system C compiler for the full path to libgcc.a
+execute_process(
+    COMMAND gcc -print-libgcc-file-name
+    OUTPUT_VARIABLE libgcc_path
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    COMMAND_ERROR_IS_FATAL ANY
+)
+
+# 2. Get the directory containing that file (this directory also contains crtbegin.o)
+get_filename_component(gcc_libs_and_obj_dir "${libgcc_path}" DIRECTORY)
+
+# 3. Apply the flags using the discovered path
+if(NOT gcc_libs_and_obj_dir)
+    message(FATAL "Could not determine GCC internal library path.")
+endif()
+
+# Set linker flags for executables, so etc
+set(CMAKE_EXE_LINKER_FLAGS    "${CMAKE_EXE_LINKER_FLAGS} -L${gcc_libs_and_obj_dir} -B${gcc_libs_and_obj_dir}")
+set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -L${gcc_libs_and_obj_dir} -B${gcc_libs_and_obj_dir}")
+set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -L${gcc_libs_and_obj_dir} -B${gcc_libs_and_obj_dir}")
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
--- deps/rules/linux-build.cmake
+++ deps/rules/linux-build.cmake
@@ -1,18 +1,13 @@
 # 1. how to use:> CXX=clang++ cmake -P linux-build.cmake
 cmake_minimum_required(VERSION 4.2)
-# edit flags as you need
-set(CMAKE_CXX_FLAGS "-stdlib=libc++")
-set(CMAKE_EXE_LINKER_FLAGS "-L/usr/lib64/gcc/x86_64-alt-linux/13/ -B/usr/lib64/gcc/x86_64-alt-linux/13/")
-set(CMAKE_SHARED_LINKER_FLAGS "-L/usr/lib64/gcc/x86_64-alt-linux/13/ -B/usr/lib64/gcc/x86_64-alt-linux/13/")
+
+get_filename_component(toolchain_path "${CMAKE_CURRENT_LIST_DIR}/../../cmake/toolchain/linux.cmake" ABSOLUTE)
 
 execute_process(COMMAND ${CMAKE_COMMAND} -B ${CMAKE_CURRENT_LIST_DIR}/build -S ${CMAKE_CURRENT_LIST_DIR}
                         -G Ninja
-                        -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
-                        -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
+                        -DCMAKE_TOOLCHAIN_FILE=${toolchain_path}
                         -DCMAKE_LINKER_TYPE=LLD
                         -DCMAKE_BUILD_TYPE=RelWithDebInfo
-                        -DCMAKE_EXE_LINKER_FLAGS=${CMAKE_EXE_LINKER_FLAGS}
-                        -DCMAKE_SHARED_LINKER_FLAGS=${CMAKE_SHARED_LINKER_FLAGS}
                 WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
                 COMMAND_ERROR_IS_FATAL ANY)
 execute_process(COMMAND ${CMAKE_COMMAND} --build ${CMAKE_CURRENT_LIST_DIR}/build --config RelWithDebInfo
