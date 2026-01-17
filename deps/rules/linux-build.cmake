8c8f0cda5af57d4249cdb2ebadb6c42667253ec9
leanid.chaika <leanid.chaika@gmail.com>
2026-01-17 19:58:27 +0300

    set linker flags to compile without error boost

--- deps/rules/linux-build.cmake
+++ deps/rules/linux-build.cmake
@@ -1,8 +1,9 @@
 # 1. how to use:> CXX=clang++ cmake -P linux-build.cmake
 cmake_minimum_required(VERSION 4.2)
 # edit flags as you need
-set(CMAKE_CXX_FLAGS "-stdlib=libc++ -L/usr/lib64/gcc/x86_64-alt-linux/13/ -B/usr/lib64/gcc/x86_64-alt-linux/13/")
-set(CMAKE_C_FLAGS "-L/usr/lib64/gcc/x86_64-alt-linux/13/ -B/usr/lib64/gcc/x86_64-alt-linux/13/")
+set(CMAKE_CXX_FLAGS "-stdlib=libc++")
+set(CMAKE_EXE_LINKER_FLAGS "-L/usr/lib64/gcc/x86_64-alt-linux/13/ -B/usr/lib64/gcc/x86_64-alt-linux/13/")
+set(CMAKE_SHARED_LINKER_FLAGS "-L/usr/lib64/gcc/x86_64-alt-linux/13/ -B/usr/lib64/gcc/x86_64-alt-linux/13/")
 
 execute_process(COMMAND ${CMAKE_COMMAND} -B ${CMAKE_CURRENT_LIST_DIR}/build -S ${CMAKE_CURRENT_LIST_DIR}
                         -G Ninja
@@ -10,6 +11,8 @@ execute_process(COMMAND ${CMAKE_COMMAND} -B ${CMAKE_CURRENT_LIST_DIR}/build -S $
                         -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
                         -DCMAKE_LINKER_TYPE=LLD
                         -DCMAKE_BUILD_TYPE=RelWithDebInfo
+                        -DCMAKE_EXE_LINKER_FLAGS=${CMAKE_EXE_LINKER_FLAGS}
+                        -DCMAKE_SHARED_LINKER_FLAGS=${CMAKE_SHARED_LINKER_FLAGS}
                 WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
                 COMMAND_ERROR_IS_FATAL ANY)
 execute_process(COMMAND ${CMAKE_COMMAND} --build ${CMAKE_CURRENT_LIST_DIR}/build --config RelWithDebInfo
