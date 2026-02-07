dcd50926e90b3c15372437087f4bae74f7b89209
leanid.chaika <leanid.chaika@gmail.com>
2026-02-07 23:25:56 +0300

    simplify octave example

--- 07-octave/02-matrix/matrix_calc.m
+++ 07-octave/02-matrix/matrix_calc.m
@@ -2,21 +2,23 @@
 % variable scale = 2
 # Octave is 1-indexed. Matrix elements are accessed as matrix(rowNum, columnNum)
 scale = 2; % print nothing cause ends with ;
+disp("scale is:"), disp(scale);
 scale*scale+scale % print 6
 
 disp(100500) % print 100500
 
-x = 3/4 * pi;
+x = 3/4 * pi; # after ';' no text output!
+x
 y = sin(x)
 
 
 scale_mat = [scale 0;
-             0 scale];
-disp(scale_mat);
-disp(scale_mat[1,1]);
+             0 scale]
+# disp(scale_mat);
+disp("matrix element access: scale_mat(1,1)"), disp(scale_mat(1,1));
 
 p = [1;
-     1];
+     1]
 scale_mat * p
 
 
@@ -25,13 +27,13 @@ step_y = 0.74;
 
 move_mat = [1 0 step_x;
             0 1 step_y;
-            0 0 1];
-disp(move_mat);
+            0 0 1]
+# disp(move_mat);
 
 p3 = [p(1); p(2); 1]; % indexes in matrix start from 1
 move_mat * p3
 
-function rot_mat = make_rot(angle)
+function rot_mat = make_rot2d(angle)
   rot_mat = [cos(angle) -sin(angle) 0;
              sin(angle) cos(angle) 0;
              0          0           1];
@@ -40,9 +42,10 @@ endfunction
 output_precision(2);
 format('short');
 
-angle = pi/2;
-disp(make_rot(angle));
-make_rot(angle) * [1;
-                   0;
-                   1]
-% expected output [0; 1; 1]
+angle = pi/2
+disp("in degrees: "), disp(rad2deg(angle));
+disp("rotation matrix 2d:"), disp(make_rot2d(angle));
+point = [1;
+         0;
+         0]
+make_rot2d(angle) * point % expected output [0; 1; 0]
