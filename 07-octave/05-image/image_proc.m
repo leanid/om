c3b23d82d5f5d7329664c67539d9daf3cb350334
leanid chaika <leanid.chaika@gmail.com>
2026-02-08 16:45:25 +0300

    add render line example

--- 07-octave/05-image/image_proc.m
+++ 07-octave/05-image/image_proc.m
@@ -5,12 +5,18 @@ printf("pixel min: %d\n", min(I(:)))
 printf("pixel max: %d\n", max(I(:)))
 printf("pixel mean: %d\n", mean(I(:)))
 
-hold ("on");
 imshow(I)
+
+hold ("on");
+% 2. Определите координаты линии (x1, y1) -> (x2, y2)
+x = [10, 200]; % Координаты по горизонтали
+y = [50, 300]; % Координаты по вертикали
+
+% 3. Нарисуйте зеленую линию
+plot(x, y, 'g', 'LineWidth', 2);
 # generate random numbers 0..1
 R = rand (size(I));
-[row, col] = find (R > 0.99);
-# imshow (R);
+[row, col] = find (R > 0.999);
 # render red Ooous over stalin photo
 plot (col, row, "ro");
 hold ("off");
