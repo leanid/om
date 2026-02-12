6604e080ee92a187ce5778ad84e9269876831935
leanid chaika <leanid.chaika@gmail.com>
2026-02-12 17:30:18 +0300

    simplify stb-config

--- cmake/stb-config.cmake
+++ cmake/stb-config.cmake
@@ -1,6 +1,7 @@
 # stb (header-only) package config.
 # Exposes imported interface target: stb::stb
-find_path(stb_INCLUDE_DIR
+find_path(
+    stb_INCLUDE_DIR
     NAMES stb/stb_image.h
     PATHS ${CMAKE_PREFIX_PATH}
     PATH_SUFFIXES include
@@ -9,8 +10,10 @@ find_path(stb_INCLUDE_DIR
 if(NOT stb_INCLUDE_DIR)
     set(stb_FOUND FALSE)
     if(stb_FIND_REQUIRED)
-        message(FATAL_ERROR
-            "stb was not found. Build deps first: cd deps/rules && CXX=clang++ cmake -P linux-build.cmake")
+        message(
+            FATAL_ERROR
+                "stb was not found. Build deps first: cd deps/rules && CXX=clang++ cmake -P linux-build.cmake"
+            )
     endif()
     return()
 endif()
@@ -18,8 +21,6 @@ endif()
 set(stb_FOUND TRUE)
 
 if(NOT TARGET stb::stb)
-    add_library(stb INTERFACE IMPORTED)
-    set_target_properties(stb PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES "${stb_INCLUDE_DIR}")
-    add_library(stb::stb ALIAS stb)
+    add_library(stb::stb INTERFACE IMPORTED)
+    target_include_directories(stb::stb INTERFACE "${stb_INCLUDE_DIR}")
 endif()
