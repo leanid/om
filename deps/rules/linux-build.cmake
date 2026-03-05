e7792050fce055caf3e09b4b87b880e29aa5c389
leanid.chaika <leanid.chaika@gmail.com>
2026-03-05 22:00:40 +0300

    format file

--- deps/rules/linux-build.cmake
+++ deps/rules/linux-build.cmake
@@ -1,15 +1,19 @@
 # 1. how to use:> CXX=clang++ cmake -P linux-build.cmake
 cmake_minimum_required(VERSION 4.2)
 
-get_filename_component(toolchain_path "${CMAKE_CURRENT_LIST_DIR}/../../cmake/toolchain/linux.cmake" ABSOLUTE)
+get_filename_component(
+    toolchain_path
+    "${CMAKE_CURRENT_LIST_DIR}/../../cmake/toolchain/linux.cmake" ABSOLUTE)
 
-execute_process(COMMAND ${CMAKE_COMMAND} -B ${CMAKE_CURRENT_LIST_DIR}/build -S ${CMAKE_CURRENT_LIST_DIR}
-                        -G Ninja
-                        -DCMAKE_TOOLCHAIN_FILE=${toolchain_path}
-                        -DCMAKE_LINKER_TYPE=LLD
-                        -DCMAKE_BUILD_TYPE=RelWithDebInfo
-                WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
-                COMMAND_ERROR_IS_FATAL ANY)
-execute_process(COMMAND ${CMAKE_COMMAND} --build ${CMAKE_CURRENT_LIST_DIR}/build --config RelWithDebInfo
-                WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
-                COMMAND_ERROR_IS_FATAL ANY)
+execute_process(
+    COMMAND
+        ${CMAKE_COMMAND} -B ${CMAKE_CURRENT_LIST_DIR}/build -S
+        ${CMAKE_CURRENT_LIST_DIR} -G Ninja
+        -DCMAKE_TOOLCHAIN_FILE=${toolchain_path} -DCMAKE_LINKER_TYPE=LLD
+        -DCMAKE_BUILD_TYPE=RelWithDebInfo
+    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR} COMMAND_ERROR_IS_FATAL ANY)
+execute_process(
+    COMMAND
+        ${CMAKE_COMMAND} --build ${CMAKE_CURRENT_LIST_DIR}/build --config
+        RelWithDebInfo
+    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR} COMMAND_ERROR_IS_FATAL ANY)
