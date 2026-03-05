12d96017757cf81ca3e0f94435f0df0cea3c0827
Leanid Chaika <leanid.chaika@gmail.com>
2026-03-05 19:52:37 +0300

    more compiler info for deps

--- deps/rules/windows-build.cmake
+++ deps/rules/windows-build.cmake
@@ -2,16 +2,25 @@
 #    With clang-cl (recommended): add LLVM to PATH, set CC=clang CXX=clang++
 #    With MSVC: run from "x64 Native Tools Command Prompt for VS"
 #    Set OM_USE_LLD=0 to skip LLD when using MSVC without LLVM
+# 2. on PowerShell set environment vars like:
+#    $ENV:CC = "clang"
+#    $ENV:CXX = "clang++"
 cmake_minimum_required(VERSION 4.2)
 
 get_filename_component(
     toolchain_path
     "${CMAKE_CURRENT_LIST_DIR}/../../cmake/toolchain/windows.cmake" ABSOLUTE)
 
+# on windows several compilers can be used:
+# 1. Ninja - -G Ninja (with CC=clang CXX=clang++)
+#   (remove MSYS2/MinGW from path to stom conflicting msvc runtime with mingw64)
+# 2. MSVC - -G "Visual Studio 17 2022" -T v143
+# 3. MSVC - -G "Visual Studio 18 2026" -T v145
+
 execute_process(
     COMMAND
         ${CMAKE_COMMAND} -B ${CMAKE_CURRENT_LIST_DIR}/build -S
-        ${CMAKE_CURRENT_LIST_DIR} -G Ninja
+        ${CMAKE_CURRENT_LIST_DIR} -G "Visual Studio 17 2022" -T v143
         -DCMAKE_TOOLCHAIN_FILE=${toolchain_path}
         -DCMAKE_BUILD_TYPE=RelWithDebInfo
     WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR} COMMAND_ERROR_IS_FATAL ANY)
