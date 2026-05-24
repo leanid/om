52c446f0e7c540b560d119b2eeebb71b45c00e16
leanid chaika <leanid.chaika@gmail.com>
2026-05-24 18:54:18 +0300

    add tex_uv works without texture

--- 02-vulkan/11-vk-texture/main.cxx
+++ 02-vulkan/11-vk-texture/main.cxx
@@ -152,10 +152,10 @@ int main_cant_throw(int argc, char** argv)
 
         // clang-format off
         std::vector<vertex> mesh_verticles = {
-            {.pos{ 0.4f, -0.4f, 0.0f }, .col{1.0f, 0.0f, 0.0f}},
-            {.pos{ 0.4f, 0.4f, 0.0f },  .col{0.0f, 1.0f, 0.0f}},
-            {.pos{ -0.4f, 0.4f, 0.0f }, .col{0.0f, 0.0f, 1.0f}},
-            {.pos{ -0.4f, -0.4f, 0.0f}, .col{1.0f, 1.0f, 0.0f}},
+            {.pos{ 0.4f, -0.4f, 0.0f }, .col{1.0f, 0.0f, 0.0f}, .tex{1.0f, 0.f}},
+            {.pos{ 0.4f, 0.4f, 0.0f },  .col{0.0f, 1.0f, 0.0f}, .tex{0.0f, 1.f}},
+            {.pos{ -0.4f, 0.4f, 0.0f }, .col{0.0f, 0.0f, 1.0f}, .tex{1.0f, 1.f}},
+            {.pos{ -0.4f, -0.4f, 0.0f}, .col{1.0f, 1.0f, 0.0f}, .tex{0.0f, 0.f}},
         };
         std::vector<std::uint16_t> mesh_indexes = {
             0, 1, 2, 2, 3, 0
