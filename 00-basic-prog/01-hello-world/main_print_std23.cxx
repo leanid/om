0306d6b4f68f5398d0b1b3fefc6360a15884b322
leanid chaika <leanid.chaika@gmail.com>
2026-06-16 22:45:37 +0300

    fix bad c++ example

--- 00-basic-prog/01-hello-world/main_print_std23.cxx
+++ 00-basic-prog/01-hello-world/main_print_std23.cxx
@@ -3,14 +3,19 @@
 
 int main()
 {
-    try
+    // по умолчанию std::println пишет в С-шный поток stdout
+    std::println(
+        "main_print_std23.cxx hello world form c++23 #include <print>");
+    // std::fflush сбрасывает буфер на уровень ОС.
+    // Если ОС (например, драйвер /dev/video0) вернет ошибку, fflush вернет
+    // EOF. Также ferror(stdout) проверит, не выставлялся ли флаг ошибки
+    // потока ранее.
+    if (std::fflush(stdout) == EOF || std::ferror(stdout))
     {
-        std::println(
-            "main_print_std23.cxx hello world form c++23 #include <print>");
-        return EXIT_SUCCESS;
-    }
-    catch (...)
-    {
+        // Выводим системную ошибку в поток std::stderr (так как stdout
+        // сломан)
+        std::perror("Error writing to stdout");
         return EXIT_FAILURE;
     }
+    return EXIT_SUCCESS;
 }
